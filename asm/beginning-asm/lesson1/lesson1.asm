8123b3238c4917c0f2a5cf50bd821e64b03187b3
Maxim Hrunenko <maxhrunenko@gmail.com>
2023-11-15 12:41:19 +0200

    lessons 4 and 5

--- asm/beginning-asm/lesson1/lesson1.asm
+++ asm/beginning-asm/lesson1/lesson1.asm
@@ -11,8 +11,9 @@ main:
     mov rdi, 1  ; 1 = to stdout
     mov rsi, msg  ; string to display on rsi
     mov rdx, len ; length of the string, wighout 0
-    syscall     ; display the string
+    syscall
+
     mov rax, 60 ; 60 = exit
     mov rdi, 0  ; 0 = success exit code
-    syscall    ; quit
+    syscall
 
