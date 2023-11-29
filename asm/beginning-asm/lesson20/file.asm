36d5d16648f88d7b76e100e0ae74c78f4a51b907
Maxim Hrunenko <maxhrunenko@gmail.com>
2023-11-29 23:41:47 +0200

    lesson20 in process

--- asm/beginning-asm/lesson20/file.asm
+++ asm/beginning-asm/lesson20/file.asm
@@ -58,4 +58,130 @@ section .data
     success_Read    db  "Reading file", NL, 0
     success_Position    db  "Positioning in file", NL, 0
 
+section .bss
+    buffer  resb    bufferlen
+section .text
+    global main
+
+main:
+push    rbp
+mov     rbp, rsp
+
+%IF CREATE
+;creating and opening file, than closing
+;creating and opening
+    mov     rdi, fileName
+    call    createFile
+    mov     qword[FD], rax; save discriptor
+;writing in file #1
+    mov     rdi, qword [FD]
+    mov     rsi, text1
+    mov     rdx, qword[len1]
+    call    writeFile
+;closing file
+    mov     rdi, qword[FD]
+    call    closeFile
+%ENDIF
+%IF OVERWRITE
+;opening and rewriting file, than cloging
+;opening file
+    mov     rdi, fileName
+    call    openFile
+    mov     qword[FD], rax
+;writing in file #2  - Rewriting (overwrite!)
+    mov     rdi, qword[FD]
+    mov     rsi, text2
+    mov     rdx, qword[len2]
+    call    writeFile
+;closing file
+    mov     rdi, qword[FD]
+    call    closeFile
+%ENDIF
+%IF APPEND
+;opening and appending in file, than closing
+;open and appending data in file
+    mov     rdi, fileName
+    call    appendFile
+    mov     qword[FD], rax
+;writing in file - APPENDING
+    mov     rdi, qword[FD]
+    mov     rsi, text3
+    mov     rdx, qword[len3]
+    call    writeFile
+    mov     rdi, qword[FD]
+    call    closeFile
+%ENDIF
+
+%IF O_WRITE
+;opening and rewriting in position in file, than closing
+;opening file for writing
+    mov     rdi, fileName
+    call    openFile
+    mov     qword[FD], rax
+;position in file at offset
+    mov     rdi, qword[FD]
+    mov     rsi, qword[len2]
+    mov     rdx, 0
+    call    positionFile
+;writing in fill position with offset
+    mov     rdi, qword[FD]
+    mov     rsi, text4
+    mov     rdx, qword[len4]
+    call    writeFile
+;closing file
+    mov     rdi, qword[FD]
+    call    closeFile
+%ENDIF
+
+%IF READ
+;opening file and reading from file, than closin
+;opening file for reading
+    mov     rdi, fileName
+    call    openFile
+    mov     qword[FD], rax
+;reading from file
+    mov     rdi, qword[FD]
+    mov     rsi, buffer
+    mov     rdx, bufferlen
+    call    readFile
+    mov     rdi, rax
+    call    printString
+;closing file
+    mov     rdi, qword[FD]
+    call    closeFile
+%ENDIF
+
+%IF O_READ
+;opening and reading in position, with offset, than closing
+;opening file
+    mov     rdi, fileName
+    call    openFile
+    mov     qword[FD], rax
+;position in file at offset
+    mov     rdi, qword[FD]
+    mov     rsi, qword[len2]
+    mov     rdx, 0
+    call    positionFile
+;read from file in position
+    mov     rdi, qword[FD]
+    mov     rsi, buffer
+    mov     rdx, 10
+    call    readFile
+    mov     rdi, rax
+    call    printString
+;closing file
+    mov     rdi, qword[FD]
+    call    closeFile
+%IF DELETE
+;deleting file
+    mov     rdi, fileName
+    call    deleteFile
+%ENDIF
+leave
+ret
+
+;file manipulation functions
+
+
+
 
