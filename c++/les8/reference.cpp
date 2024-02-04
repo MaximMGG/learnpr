7de93b24ffb992de05be1a67c9d97a8ef1a90a1e
maximhrunenko <maxhrunenko@gmail.com>
2024-02-04 17:32:11 +0200

    templates and functions polymorphism

--- c++/les8/reference.cpp
+++ c++/les8/reference.cpp
@@ -1,5 +1,18 @@
 #include <iostream>
 
+
+typedef struct {
+    float x, y;
+} vec;
+
+char *left(vec v = {.x = 1, .y = 2}, int i = 1) {
+
+    return nullptr;
+}
+
+char *left(const char * str, double x);
+
+
 int calc(int& a, int& b) {
     a += b;
     for(int i = 1; i < 4; i++)
@@ -9,13 +22,14 @@ int calc(int& a, int& b) {
 }
 
 
-
 int main() {
     int arr[4] {1, 2, 3, 4};
     int arr2[4] {1, 2, 3, 4};
 
     int c = calc(arr[0], arr2[0]);
-
+    left({.x = 2, .y = 6}, 34);
+    left("Hello", 0);
+    left();
 
     std::cout << c << std::endl;
     return 0;
