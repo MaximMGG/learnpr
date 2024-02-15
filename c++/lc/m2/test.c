5bfaf70eb7591430af786db4530aadf182ae3067
maximhrunenko <maxhrunenko@gmail.com>
2024-02-15 16:01:11 +0200

    memory tricks

---
+++ c++/lc/m2/test.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+struct app {
+    int x;
+    int y;
+    long z;
+    char *name;
+};
+
+typedef struct point point;
+
+
+int main() {
+
+    void *p = malloc(24);
+    void *P = p;
+    int x = 12;
+    int y = 213421;
+    long z = 1511111111111111;
+    char *name = "Sham";
+    memcpy(P, &x, 4);
+    memcpy((P + 4), &y, 4);
+    memcpy((P + 8), &z, 8);
+    memcpy((P + 16), name, 8);
+    struct app *a = malloc(sizeof(struct app));
+
+    a->x = *(int *) P;
+    a->y = *(int *) (P + 4);
+    a->z = *(long *) (P + 8);
+    a->name = (char *) (P + 16);
+
+    printf("x = %d, y = %d, z = %ld, name = %s\n", a->x, a->y, a->z, a->name);
+
+    free(p);
+    free(a);
+
+    return 0;
+}
