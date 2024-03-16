359e3ea932fca78234f9dd3844f8557ccf4ed695
maximhrunenko <maxhrunenko@gmail.com>
2024-03-16 14:21:14 +0200

    calc app, practise with tokens writing

---
+++ c/calc_app/src/app.c
@@ -0,0 +1,167 @@
+#include <stdio.h>
+#include "../headers/calc.h"
+#include <string.h>
+
+static Token *set_left_token(Token **tokens, int i, int *pos) {
+    i--;
+    while(1) {
+        if (tokens[i] == NULL) {
+            i--;
+            if (i < 0) {
+                return NULL;
+            }
+        } else {
+            *pos = i;
+            return tokens[i];
+        }
+    }
+}
+
+static Token *set_right_token(Token **tokens, int i, int max_size, int *pos) {
+    i++;
+    while(1) {
+        if (tokens[i] == NULL) {
+            if (i >= max_size) {
+                return NULL;
+            }
+            i++;
+        } else {
+            *pos = i;
+            return tokens[i];
+        }
+    }
+}
+
+int main(int argc, char **argv) {
+
+    if (argc < 2) {
+        perror("argc < 2");
+    }
+
+    LinkedList *list = tokinize_string(argv[1]);
+    Literator *it = LList_get_iterator(list);
+
+    char calc = 1;
+    int it_c = 0;
+    int mult = 0;
+    int div = 0;
+    int sum = 0;
+    int sub = 0;
+
+    while(it->data != NULL) {
+        switch (((Token *)it->data)->type) {
+            case '+':
+                sum++;
+                break;
+            case '-':
+                sub++;
+                break;
+            case '*':
+                mult++;
+                break;
+            case '/':
+                div++;
+                break;
+
+        }
+        LList_iterator_next(it);
+        it_c++;
+    }
+
+    Token **tokens = (Token **) malloc(sizeof(Token *) * it_c);
+
+
+    it = LList_get_iterator(list);
+    for(int i = 0; i < it_c; i++) {
+        tokens[i] = (Token *)it->data;
+        LList_iterator_next(it);
+    }
+
+    int token_count = it_c;
+    int token_x_pos = 0;
+    int token_y_pos = 0;
+
+    while(calc) {
+        int i = 0;
+        if (token_count == 1) {
+            break;
+        }
+        while(i < it_c) {
+            if (tokens[i] == NULL) {
+                i++;
+                continue;
+            } 
+            if (tokens[i]->type == '*') {
+                Token *x = set_left_token(tokens, i, &token_x_pos);
+                Token *y = set_right_token(tokens, i, it_c, &token_y_pos);
+                if (y == NULL) {
+                    calc = 0;
+                    break;
+                }
+                x->value *= y->value;
+                tokens[token_x_pos] = NULL;
+                tokens[token_y_pos] = NULL;
+                token_count -= 2;
+                mult--;
+                i++;
+                continue;
+            } else if (tokens[i]->type == '/') {
+                Token *x = set_left_token(tokens, i, &token_x_pos);
+                Token *y = set_right_token(tokens, i, it_c, &token_y_pos);
+                if (y == NULL) {
+                    calc = 0;
+                    break;
+                }
+                x->value /= y->value;
+                tokens[token_x_pos] = NULL;
+                tokens[token_y_pos] = NULL;
+                token_count -= 2;
+                div--;
+                i++;
+                continue;
+            }
+            if (mult <= 0 && div <= 0) {
+                if (tokens[i]->type == '+') {
+                    Token *x = set_left_token(tokens, i, &token_x_pos);
+                    Token *y = set_right_token(tokens, i, it_c, &token_y_pos);
+                    if (y == NULL) {
+                        calc = 0;
+                        break;
+                    }
+                    x->value += y->value;
+                    tokens[token_x_pos] = NULL;
+                    tokens[token_y_pos] = NULL;
+                    token_count -= 2;
+                    sum--;
+                    i++;
+                    continue;
+                } else if (tokens[i]->type == '-') {
+                    Token *x = set_left_token(tokens, i, &token_x_pos);
+                    Token *y = set_right_token(tokens, i, it_c, &token_y_pos);
+                    if (y == NULL) {
+                        calc = 0;
+                        break;
+                    }
+                    x->value -= y->value;
+                    tokens[token_x_pos] = NULL;
+                    tokens[token_y_pos] = NULL;
+                    token_count -= 2;
+                    sub--;
+                    i++;
+                    continue;
+                }
+            }
+            i++;
+        }
+    }
+
+
+    Token *t = tokens[0];
+    printf("%lf\n", t->value);
+
+
+    LList_iterator_free(it);
+    // LList_destroy_list(list, NULL);
+    free(tokens);
+    return 0;
+}
---
+++ c/calc_app/src/calc.c
@@ -0,0 +1,55 @@
+#include "../headers/calc.h"
+#include <string.h>
+
+
+
+LinkedList *tokinize_string(const char *expression) {
+    LinkedList *list = LList_create(L_STRUCT);
+
+    int e_len = strlen(expression);
+    double temp_v;
+    char type;
+    char buf[32];
+    
+    for(int i = 0; i < e_len; i++) {
+        if (expression[i] >= '0' && expression[i] <= '9' || expression[i] == '.') {
+            type = '8';
+            for(int j = 0; j < e_len; j++, i++) {
+                buf[j] = expression[i];
+                if (expression[i + 1] == ' ' || expression[i + 1] < '0' || expression[i + 1] > '9') {
+                    goto next_sykle;
+                }
+            }
+        }
+        switch (expression[i]) {
+            case '+':
+                type = '+';
+                goto next_sykle;
+            case '-':
+                type = '-';
+                goto next_sykle;
+            case '*':
+                type = '*';
+                goto next_sykle;
+            case '/':
+                type = '/';
+                goto next_sykle;
+        }
+        if (expression[i] == ' ') {
+            continue;
+        }
+next_sykle:
+        if (type == '8') {
+            Token t = {.type = type, .value = atof(buf)};
+            LList_append_next(list, &t, sizeof(Token));
+            type = 0;
+            memset(buf, 0, 32);
+        } else {
+            Token t = {.type = type};
+            LList_append_next(list, &t, sizeof(Token));
+            type = 0;
+            memset(buf, 0, 32);
+        }
+    }
+    return list;
+}
