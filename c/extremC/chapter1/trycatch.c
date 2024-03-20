ca51c9c57b5000e9f2ad1fd88847337b6ee1d2d7
maximhrunenko <maxhrunenko@gmail.com>
2024-03-20 21:47:45 +0200

    setjmp and longjmp funcs

--- c/extremC/chapter1/trycatch.c
+++ c/extremC/chapter1/trycatch.c
@@ -1,9 +1,11 @@
 #include "errort.h"
 #include <stdio.h>
 #include <string.h>
+#include <setjmp.h>
 
 typedef struct {
     char inside;
+    jmp_buf jb;
 }try_block;
 
 try_block b;
@@ -11,10 +13,10 @@ try_block b;
 
 #define TRY b.inside = 1;
 #define CATCH(exeption)  \
-            catch1:             \
-            if ( _ ## exeption ## _t.active)
+            if (setjmp(b.jb)) {     \
+                if ( _ ## exeption ## _t.active)    \
 
-#define THROW(error, m) _ ## error ## _t.active = 1; if (m == NULL) {strcpy(_ ## error ## _t.msg, "error");} else strcpy(_ ## error ## _t.msg, m); 
+#define THROW(error, m) _ ## error ## _t.active = 1; if (m == NULL) {strcpy(_ ## error ## _t.msg, "error");} else {strcpy(_ ## error ## _t.msg, m);}  if (b.inside) longjmp(b.jb, 0);
 #define E(exeption) _ ## exeption ## _t
 
 
