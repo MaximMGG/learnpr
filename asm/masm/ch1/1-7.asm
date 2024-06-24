bc5d7236ca372c7ec66d12f4eb871f40302fa27d
MaximMGG <maxhrunenko@gmail.com>
2024-06-24 22:26:23 +0300

    1-7 listening done

--- asm/masm/ch1/1-7.asm
+++ asm/masm/ch1/1-7.asm
@@ -1,21 +1,41 @@
-    option casemap:none
+option casemap:none
 
-    nl = 10
-    maxLen = 256
+nl = 10
+maxLen = 256
 
-    .data
+.data
     titleStr    byte    "Listen 1-7", 0
     prompt      byte    "Enter a string: ", 0
     fmtStr      byte    "User entered: '%s'", nl, 0
-
 
     input   byte    maxLen  dup (?)
 
 .code
 externdef   printf:proc
 externdef   readLine:proc
+
 public getTitle
 getTitle PROC
-    lea     rax, titlestr
+    lea     rax, titleStr
     ret
 getTitle ENDP
+
+public asmMain
+asmMain PROC
+    sub     rsp, 56
+    lea     rcx, prompt
+    call    printf
+    mov     input, 0
+
+    lea     rcx, input
+    mov     rdx, maxLen
+    call    readLine
+
+    lea     rcx, fmtStr
+    lea     rdx, input
+    call    printf
+    add     rsp, 56
+    ret
+asmMain ENDP 
+
+END
