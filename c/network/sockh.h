503a22519a71e4af0e93a4a5e8c0119cb3131463
Maxim Hrunenko <maxhrunenko@gmail.com>
2024-07-07 16:48:42 +0300

    universal headers

---
+++ c/network/sockh.h
@@ -0,0 +1,38 @@
+#ifndef SOCKH_H
+#define SOCKH_H
+#endif
+
+#ifdef __WIN32
+#ifndef _WIN32_WINNT
+#define _WIN32_WINNT 0x0600
+#endif
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#pragma comment(lib, "ws2_32.lib")
+
+#else
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <unistd.h>
+#include <netdb.h>
+#include <errno.h>
+#endif
+
+
+#ifdef __WIN32
+
+#define ISVALIDSOCKET(s) ((s) != INVALID_SOCKET)
+#define CLOSESOCKET(s) closesocket(s)
+#define GETSOCKETERRNO() (WSAGetLastError())
+#else
+#define ISVALIDSOCKET(s) ((s) >= 0)
+#define CLOSESOCKET(s) close(s)
+#define SOCKET int
+#define GETSOCKETERRNO() (errno)
+#endif
+
+#include <string.h>
+#include <stdio.h>
+
