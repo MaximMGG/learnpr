c1c24b752c0b6f09a952cd3c3369a2c3ef6b9a43
Maxim Hrunenko <maxhrunenko@gmail.com>
2024-07-28 18:32:38 +0300

    init linuxapi

---
+++ linuxapi/error_functions.h
@@ -0,0 +1,19 @@
+#ifndef ERROR_FUNCTION_H
+#define ERROR_FUNCTION_H
+
+#ifdef __GNUC__
+#define NORETURN __attribute__ ((__noreturn__))
+#else
+#define NORETURN
+#endif
+
+void errMsg(const char *format) NORETURN;
+void errExit(const char *format, ...) NORETURN;
+void err_exit(const char *format, ...) NORETURN;
+void errExitEN(int errnum, const char *format, ...) NORETURN;
+void fatal(const char *format, ...) NORETURN;
+void usageErr(const char *format, ...) NORETURN;
+void cmdLineErr(const char *format, ...) NORETURN;
+
+
+#endif
