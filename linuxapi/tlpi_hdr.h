f32776fe3ff40b5d366add5d25b5f73eb5735b90
Maxim Hrunenko <maxhrunenko@gmail.com>
2024-07-29 22:41:50 +0300

    get_num.c && get_num.h

--- linuxapi/tlpi_hdr.h
+++ linuxapi/tlpi_hdr.h
@@ -13,9 +13,42 @@
 
 #include "error_functions.h"
 
+#ifdef TRUE
+#undef TRUE
+#endif
+#ifdef FALSE
+#undef FALSE
+#endif
+
 typedef enum {FALSE, TRUE} Boolean;
 
 #define min(m, n) ((m) < (n) ? (m) : (n))
 #define max(m, n) (((m) < (n) ? (n) : (m)))
 
-#endif //TLPI_HDR_H
+#ifdef __sgi
+typedef int socklen_t
+#endif
+
+#ifdef __sun
+#include <sys/file.h>
+#endif
+
+#if defined(O_ASYNC) && defined(FASYNC)
+#define O_ASYNC FASYNC
+#endif
+
+#if defined(MAP_ANON) && ! defined(MAP_ANONYMOUS)
+#define  MAP_ANONYMOUS MAP_ANON
+#endif 
+
+#if ! defined(O_SYNC) && defined(O_FSYNC)
+#define O_SYNC O_FSYNC
+#endif
+
+#ifdef __FreeBSD__
+#define sival_int sigval_int
+#define sival_ptr sigval_ptr
+#endif
+
+
+#endif //TLPI_HDR_H 
