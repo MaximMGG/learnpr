17ed8501952eac4b8067dc67443f793b98874d50
Maxim Hrunenko <maxhrunenko@gmail.com>
2024-08-04 11:38:16 +0300

    write app for deleting executables

--- hello.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-for C in erd orange yellow green blue indigo violet; do
-    echo $C
-done
---
+++ remapp/main.c
@@ -0,0 +1,73 @@
+#include <stdio.h>
+#include <string.h>
+#include <dirent.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+
+
+
+char *get_local_dir() {
+    char *cwd = malloc(sizeof(char) * 512);
+    getcwd(cwd, 512);
+    if (cwd != NULL) {
+        return cwd;
+    } else {
+        fprintf(stderr, "Cwd is null\n");
+        fflush(stderr);
+        return NULL;
+    }
+}
+
+void remove_exe(char *path) {
+    struct stat sb;
+    if (stat(path, &sb) == -1) {
+        fprintf(stderr, "error check stat for file %s\n", path);
+    }
+
+    if (S_ISREG(sb.st_mode) && (sb.st_mode & S_IXUSR)) {
+        printf("Removing file %s...\n", path);
+        remove(path);
+    }
+}
+
+
+void scan_dir(char *full_path) {
+    DIR *dir = opendir(full_path);
+    if (dir == NULL) {
+        fprintf(stderr, "Cant open dir %s\n", full_path);
+    }
+    struct dirent *dd;
+    printf("Check dir %s...\n", full_path);
+
+    while((dd = readdir(dir)) != NULL) {
+        if (strcmp(dd->d_name, ".") == 0 ||strcmp(dd->d_name, "..") == 0) {
+
+        } else {
+            if (dd->d_type == DT_DIR) {
+                char *path_cpy = malloc(sizeof(char) * 512);
+                strcpy(path_cpy, full_path);
+                strcat(path_cpy, "/");
+                strcat(path_cpy, dd->d_name);
+                scan_dir(path_cpy);
+                free(path_cpy);
+            } else if (dd->d_type == DT_REG) {
+                char pathcpy[512];
+                strcpy(pathcpy, full_path);
+                strcat(pathcpy, "/");
+                strcat(pathcpy, dd->d_name);
+                remove_exe(pathcpy);
+            }
+        }
+    }
+}
+
+
+int main() {
+    char *path = get_local_dir();
+    scan_dir(path);
+
+    free(path);
+
+    return 0;
+}
