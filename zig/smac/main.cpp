2d6977dc5daa33bc80abd9b9ad93e74658594814
MaximMGG <maxhrunenko@gmail.com>
2024-08-28 22:05:56 +0300

    continue checking

--- zig/smac/main.cpp
+++ zig/smac/main.cpp
@@ -12,20 +12,21 @@ void print_list(std::list<char *>& list) {
 }
 
 int main() {
-    std::list<char *> list;
+    std::list<char *> *list = new std::list<char *>;
     char buf[16]{0};
 
     for(int i = 0; i < 1000001; i++) {
         if(i % 10 == 7 || i % 7 == 0) {
-            list.push_back((char *)"SMAC");
+            list->push_back((char *)"SMAC");
         } else {
             sprintf(buf, "%d", i);
-            list.push_back(buf);
+            list->push_back((char *)buf);
             memset(buf, 0, 16);
         }
     }
 
-    print_list(list);
+    print_list(*list);
+    delete list;
 
     return 0;
 }
--- zig/smac/main.zig
+++ zig/smac/main.zig
@@ -2,24 +2,21 @@ const std = @import("std");
 const stdout = std.io.getStdOut().writer();
 const heap_allo = std.heap.page_allocator;
 
-
-
 pub fn main() !void {
     const X = 4;
     var t: [X]std.Thread = undefined;
 
     var list: [X]std.ArrayList(u8) = undefined;
 
-    var index:u32 = 0;
+    var index: u32 = 0;
 
-    while(index < X) : (index += 1) {
+    while (index < X) : (index += 1) {
         list[index] = std.ArrayList(u8).init(heap_allo);
         defer list[index].deinit();
-        t[index] = try std.Thread.spawn(.{}, count_it, .{index, &list[index]});
-
+        t[index] = try std.Thread.spawn(.{}, count_it, .{ index, &list[index] });
     }
     index = 0;
-    while(index < X) : (index += 1) {
+    while (index < X) : (index += 1) {
         t[index].join();
         try print_list(&list[index]);
         list[index].clearAndFree();
@@ -31,7 +28,7 @@ fn count_it(id: u32, list: *std.ArrayList(u8)) !void {
     if (id == 3) addr += 1;
 
     var index: usize = id * 250000;
-    while(index < (id * 250_000 + 250000 + addr)) : (index += 1) {
+    while (index < (id * 250_000 + 250000 + addr)) : (index += 1) {
         if (index == 0) continue;
         if (index % 10 == 7 or index % 7 == 0) {
             try list.writer().print("{s}\n", .{"SMAC"});
@@ -41,11 +38,9 @@ fn count_it(id: u32, list: *std.ArrayList(u8)) !void {
     }
 }
 
-
-
 fn print_list(list: *std.ArrayList(u8)) !void {
     var iter = std.mem.splitSequence(u8, list.items, "\n");
-    while(iter.next()) |item| {
+    while (iter.next()) |item| {
         try stdout.print("{s}\n", .{item});
     }
 }
