cbd1db2fdd495851e88ddea7c65ff5933362cad7
MaximMGG <maxhrunenko@gmail.com>
2024-08-30 19:50:53 +0300

    structs and array in zig

---
+++ zig/learnzpg/array.zig
@@ -0,0 +1,26 @@
+const std = @import("std");
+const print = std.debug.print;
+
+pub fn main() void {
+    const a = [5]i32{ 1, 2, 3, 4, 5 };
+    // const b: [5]i32 = .{ 1, 2, 3, 4, 5 };
+    // const c = [_]i32{ 1, 2, 3, 4, 5 };
+    // const b = a[0..3];
+    // _ = b;
+    const end: usize = 4;
+    const c = a[1..end];
+
+    print("{any}\n", .{@TypeOf(a)});
+
+    print("{any}\n", .{@TypeOf(a[0..])});
+    print("{any}\n", .{@TypeOf(c)});
+    // print_arr(a[0..]);
+    // print_arr(b);
+    // print_arr(c);
+}
+
+pub fn print_arr(arr: []i32) void {
+    for (arr) |a| {
+        print("{d}\n", a);
+    }
+}
