ff4052d7072cbb85e671a501b7e0a1fce72d805b
MaximMGG <maxhrunenko@gmail.com>
2024-08-31 17:01:19 +0300

    pointers in zig

--- zig/learnzpg/remexe.zig
+++ zig/learnzpg/remexe.zig
@@ -1,12 +1,15 @@
 const std = @import("std");
+const cc = @cImport(@cInclude("string.h"));
 const c = std.c;
 
 pub fn main() !void {
     const allocator = std.heap.page_allocator;
     const userName: []u8 = try allocator.alloc(u8, 512);
-    const us: [*]u8 = &userName;
     defer allocator.free(userName);
-    c.getcwd(us, 512);
+
+    _ = cc.memset(userName.ptr, 0, 512);
+    // @memset(userName, 0);
+    _ = c.getcwd(userName.ptr, 512);
 
     std.debug.print("User name is {?s}\n", .{userName});
 }
