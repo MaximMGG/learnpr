8311e8c9863a9bdbcd6856e39d43a9a319628787
MaximMGG <maxhrunenko@gmail.com>
2024-09-03 21:04:17 +0300

    bufAllocator and bufPrint

---
+++ zig/learnzig/heap/json.zig
@@ -0,0 +1,16 @@
+const std = @import("std");
+
+pub fn main() !void {
+    var buf: [150]u8 = undefined;
+    var fa = std.heap.FixedBufferAllocator.init(&buf);
+    defer fa.reset();
+
+    const allocator = fa.allocator();
+
+    const json = try std.json.stringifyAlloc(allocator, .{
+        .this_is = "an anonimous struct",
+        .above = true,
+        .last_param = "are options",
+    }, .{ .whitespace = .indent_2 });
+    std.debug.print("{s}\n", .{json});
+}
