018db3494f45570aa0676e142d68d56dcbbd8513
MaximMGG <maxhrunenko@gmail.com>
2024-09-03 20:50:46 +0300

    allocators and testing

--- zig/learnzig/heap/allocator.zig
+++ zig/learnzig/heap/allocator.zig
@@ -12,11 +12,26 @@ pub fn main() !void {
 
     levelUp(user);
     std.debug.print("User {d}, has power {d}\n", .{ user.id, user.power });
+
+    var u = try User.init(alc, 33, 10000);
+
+    std.debug.print("User {d}, has power {d}\n", .{ u.id, u.power });
+    defer u.deinit(alc);
 }
 
 pub const User = struct {
     id: u64,
     power: i32,
+
+    fn init(allocator: std.mem.Allocator, id: u64, power: i32) !*User {
+        const user = try allocator.create(User);
+        user.* = .{ .id = id, .power = power };
+        return user;
+    }
+
+    fn deinit(self: *User, allocator: std.mem.Allocator) void {
+        allocator.destroy(self);
+    }
 };
 
 fn levelUp(user: *User) void {
