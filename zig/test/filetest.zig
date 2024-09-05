87637e91d4c743e5d9c0dcf2fdece7e1176ceb99
MaximMGG <maxhrunenko@gmail.com>
2024-09-05 16:55:13 +0300

    filetest.zig

--- zig/test/filetest.zig
+++ zig/test/filetest.zig
@@ -1,33 +1,12 @@
 const std = @import("std");
-const c = std.c;
-const c_dirent = @cImport(@cInclude("dirent.h"));
 
-const _file = struct {};
+pub fn main() !void {
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    const alc = gpa.allocator();
+    var walker = try std.fs.cwd().walk(alc);
+    defer walker.deinit();
 
-pub fn main() void {
-    const file: ?*c.FILE = c.fopen("reflect.zig", "r");
-    defer _ = c.fclose(file.?);
-    var buf: [512]u8 = undefined;
-    @memset(buf[0..], 0);
-
-    if (file != null) {
-        const read_byte: usize = c.fread(&buf, 1, 512, file.?);
-        std.debug.print("Read {d} bytes\n", .{read_byte});
-        std.debug.print("file content -> {s}\n", .{buf});
-    }
-    readdir();
-}
-
-fn readdir() void {
-    const dir: ?*c_dirent.DIR = c_dirent.opendir("../");
-    defer _ = c_dirent.closedir(dir.?);
-
-    var dirent: ?*c_dirent.dirent = null;
-    var i: usize = 0;
-
-    while (i < 5) : (i += 1) {
-        dirent = c_dirent.readdir(dir.?);
-
-        std.debug.print("dir -> {s}\n", .{dirent.?.d_name});
+    while (try walker.next()) |f| {
+        std.debug.print("{any}\n", .{f});
     }
 }
