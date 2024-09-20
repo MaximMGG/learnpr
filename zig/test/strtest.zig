c26e521f3122715dfef9899480d0ec799809c733
MaximMGG <maxhrunenko@gmail.com>
2024-09-20 20:35:39 +0300

    strtest str pointer

--- zig/test/strtest.zig
+++ zig/test/strtest.zig
@@ -1,12 +1,17 @@
 const std = @import("std");
 
 pub fn main() !void {
-    var buf: [512]u8 = .{0} ** 512;
-    _ = try std.posix.getcwd(buf[0..]);
-    std.debug.print("{s}\n", .{buf});
-    const len = std.mem.len(@as([*:0]u8, @ptrCast(&buf)));
-    std.debug.print("{d}\n", .{len});
-    std.debug.print("{d}\n", .{buf.len});
-    std.mem.copyForwards(u8, buf[len..], "/");
-    std.debug.print("{s}\n", .{buf});
+    const str = "Hello";
+    const len: usize = strlen(str);
+    std.debug.print("len is {d}\n", .{len});
+}
+
+fn strlen(str: [*]const u8) usize {
+    var i: usize = 0;
+    while (i < 512) : (i += 1) {
+        if (str[i] == 0) {
+            return i;
+        }
+    }
+    return i;
 }
