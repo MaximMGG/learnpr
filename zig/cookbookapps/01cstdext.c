2584237673ecbfefd375b39b783e2ed390474348
MaximMGG <maxhrunenko@gmail.com>
2024-09-28 16:56:12 +0300

    stream read from file C

---
+++ zig/cookbookapps/01cstdext.c
@@ -0,0 +1,17 @@
+#include <cstdext/stream.h>
+
+int main() {
+    str file = "test2.txt";
+    stream *st = createStream(FILE_STREAM, file);
+
+    str s;
+    i32 count = 0;
+
+    while((s = st->nextStr(st)) != null) {
+        printf("%d - %s\n", count++, s);
+    }
+    printf("Total lines is: %d\n", count);
+    destroyStream(st);
+
+    return 0;
+}
