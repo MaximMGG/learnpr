3dce2ddb22d4ee7fb328af41df68da8b13ded52c
MaximMGG <maxhrunenko@gmail.com>
2024-10-07 23:18:43 +0300

    check

--- zig/arr.zig
+++ zig/arr.zig
@@ -4,10 +4,10 @@ const output = std.io.getStdOut();
 
 pub fn main() !void {
     var arr: [100000]i32 = undefined;
+    std.debug.print("Set hello {d}\n", .{1});
 
     for (0..100000) |i| {
         arr[i] = 2 * 5 / 1 + 11 + 33 - 99;
     }
-
     print("array size is: {}\n", .{arr.len});
 }
