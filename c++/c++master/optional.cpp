f668f677423292ee5f5e4d41c791fdf1f0be9559
MaximMGG <maxhrunenko@gmail.com>
2025-01-11 10:56:08 +0000

    function overloading

--- c++/c++master/optional.cpp
+++ c++/c++master/optional.cpp
@@ -7,9 +7,20 @@ int main() {
 
     std::optional<int> i {3};
     std::optional<std::string> s {"Danya"};
+    std::optional<int> a {};
     std::cout << i.value() << '\n';
     i.reset();
     //std::cout << i.value() << '\n';
+    std::cout << *i << '\n';
+    i.has_value();
+    if (i != std::nullopt) {
+
+    }
+
+    i.value_or(8);
+
+    std::cout << a.value() << '\n';
+
 
 
     return 0;
