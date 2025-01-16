f43c4285ffd90566770f94bf63115a87f1b6c49d
MaximMGG <maxhrunenko@gmail.com>
2025-01-16 21:12:43 +0200

    asm test

--- asm/hello.asm
+++ asm/hello.asm
@@ -1,15 +1,21 @@
-%include "stud_io.inc"
+;%include "stud_io.inc"
 global _start
 
 section .text
-_start:     mov         eax, 0
-again:      PRINT       "Hello"
-            PUTCHAR     10 
-            inc         eax
-            cmp         eax, 5
-            jl          again
+_start:     
+    push    rbp
+    mov     rbp, rsp
+
+
+
+    leave
+;again:      PRINT       "Hello"
+;            PUTCHAR     10 
+;            inc         eax
+;            cmp         eax, 5
+;            jl          again
 ;arr         db 1, 2, 3, 4, 5, 7
 ;            PRINT       arr%%
 ;            inc         arr
 ; 556
-            FINISH
+    ret
