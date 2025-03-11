a7847bac69d868ad3621666f780d7850878bfefb
MaximMGG <maxhrunenko@gmail.com>
2025-03-11 21:35:51 +0200

    Base64 encode need to refactor

--- zig/int_to_zig/chapter_4/main.zig
+++ zig/int_to_zig/chapter_4/main.zig
@@ -9,5 +9,13 @@ pub fn main() !void {
     const allocator = fba.allocator();
 
     const text = "Testing some more stuff";
-    const etext = "";
+    const etext = "VGVzdGluZyBzb21lIG1vcmUgc3R1ZmY=";
+    const b64 = base64.Base64.init();
+    const encoded_text = b64.encode(allocator, text);
+    const decoded_text = b64.decode(allocator, encoded_text);
+
+    try stdout.print("Basic text: {s}\n", .{text});
+    try stdout.print("Basic etext: {s}\n", .{etext});
+    try stdout.print("Encoded text: {s}\n", .{encoded_text});
+    try stdout.print("Decoded text: {s}\n", .{decoded_text});
 }
