eff7a53cd82bbd0ebd0e7350b5d95b704fe1b45b
MaximMGG <maxhrunenko@gmail.com>
2025-03-11 11:29:47 +0000

    merge

--- zig/int_to_zig/chapter_3/main.zig
+++ zig/int_to_zig/chapter_3/main.zig
@@ -32,6 +32,10 @@ pub fn main() !void {
     stack_example();
     try alloc_example();
     try alloc_and_read();
+    try general_pa();
+    try page_a();
+    try buffer_a();
+    try areana_a();
 }
 
 fn add(x: u8, y: u8) u8 {
@@ -71,5 +75,90 @@ fn alloc_and_read() !void {
 fn setval(comptime T: type, buf: []T, val: T) void {
     for (0..buf.len) |i| {
         buf[i] = val;
+    }
+}
+
+fn general_pa() !void {
+    try stdout.print("GeneralPurposeAllocator\n", .{});
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    defer {
+        const check: std.heap.Check = gpa.deinit();
+        switch (check) {
+            .ok => std.debug.print("gpa deinit ok\n", .{}),
+            .leak => std.debug.print("gpa leak memory\n", .{}),
+        }
+    }
+    const allocator = gpa.allocator();
+
+    const buf: []u8 = try allocator.alloc(u8, 100);
+    defer allocator.free(buf);
+    @memset(buf, 0);
+    std.mem.copyForwards(u8, buf, "Hello allocators");
+
+    try stdout.print("{s}\n", .{buf});
+}
+
+fn page_a() !void {
+    try stdout.print("page_allocator\n", .{});
+    const pa = std.heap.page_allocator;
+
+    const buf: []u8 = try pa.alloc(u8, 200);
+    @memset(buf, 0);
+    std.mem.copyForwards(u8, buf, "What a nice day)");
+
+    try stdout.print("{s}\n", .{buf});
+
+    pa.free(buf);
+}
+
+fn buffer_a() !void {
+    try stdout.print("FixedBufferAllocator\n", .{});
+    var buffer: [200]u8 = .{0} ** 200;
+
+    var fba = std.heap.FixedBufferAllocator.init(&buffer);
+    const allocator = fba.allocator();
+
+    const buf: []u8 = try allocator.alloc(u8, 20);
+    defer allocator.free(buf);
+    @memset(buf, 0);
+
+    std.mem.copyForwards(u8, buf, "Little text");
+    try stdout.print("{s}\n", .{buf});
+}
+
+fn areana_a() !void {
+    try stdout.print("areana_allocator\n", .{});
+    const c_alloc = std.heap.c_allocator;
+    var aa = std.heap.ArenaAllocator.init(c_alloc);
+    defer aa.deinit();
+    const allocator = aa.allocator();
+
+    const buf: []u8 = try allocator.alloc(u8, 50);
+    @memset(buf, 0);
+
+    std.mem.copyForwards(u8, buf, "What i want to say, I dont dow, fortunately");
+
+    try stdout.print("{s}\n", .{buf});
+
+    const i: *u64 = try allocator.create(u64);
+    i.* = 999;
+
+    try stdout.print("{d}\n", .{i.*});
+    allocator.destroy(i);
+
+    const mega_buf: []f64 = try allocator.alloc(f64, 200);
+    @memset(mega_buf, 0);
+
+    for (0..mega_buf.len) |f| {
+        mega_buf[f] = @as(f64, @floatFromInt(f)) + 0.3;
+    }
+
+    try stdout.print("{any}\n", .{mega_buf});
+
+    const mega_buf_p: [*]f64 = mega_buf.ptr;
+    const mega_buf_len: usize = mega_buf.len;
+
+    for (0..mega_buf_len) |f| {
+        try stdout.print("{d}\n", .{mega_buf_p[f]});
     }
 }
