8f96c207bb3478d6fc0786909dee8eaf547a442e
MaximMGG <maxhrunenko@gmail.com>
2025-03-15 13:36:21 +0000

    server in zig

--- zig/int_to_zig/chapter_7/main.zig
+++ zig/int_to_zig/chapter_7/main.zig
@@ -1,35 +1,15 @@
 const std = @import("std");
+const SocketConf = @import("config.zig");
+const Request = @import("request.zig");
+const stdout = std.io.getStdOut().writer();
 
 pub fn main() !void {
-    var num: u32 = 0;
-    num = return_null(4) orelse 0;
-    std.debug.print("{d}\n", .{num});
-    num = return_null(5) orelse blk: {
-        const a: u32 = 4;
-        break :blk a * 11;
-    };
-    std.debug.print("{d}\n", .{num});
-
-    o: for (0..10) |i| {
-        std.debug.print("{d}", .{i});
-        if (i == 3) {
-            break :o;
-        }
-        one: for (0..10) |j| {
-            if (j == 5) {
-                break :o;
-            }
-            if (j == 9) {
-                break :one;
-            }
-        }
-    }
-}
-
-fn return_null(num: u32) ?u32 {
-    if (num % 2 == 0) {
-        return num / 2;
-    } else {
-        return null;
-    }
+    const socket = try SocketConf.Socket.init();
+    try stdout.print("Server Addr: {any}\n", .{socket._address});
+    var server = try socket._address.listen(.{});
+    const connection = try server.accept();
+    var buffer: [1000]u8 = undefined;
+    @memset(&buffer, 0);
+    _ = try Request.read_request(connection, buffer[0..buffer.len]);
+    try stdout.print("{s}\n", .{buffer});
 }
