2aaa0efa24bd18ffbac842fdd23d96e67ddde81f
MaximMGG <maxhrunenko@gmail.com>
2025-03-23 20:36:29 +0000

    Generic version of stack

--- zig/int_to_zig/chapter_12/main.zig
+++ zig/int_to_zig/chapter_12/main.zig
@@ -1,7 +1,26 @@
 const std = @import("std");
 const expect = std.testing.expect;
+const Stack = @import("stack.zig");
 
 pub fn main() !void {
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    const allocator = gpa.allocator();
+    var s = try Stack.Stack(u32).init(allocator, 5);
+    defer s.deinit();
+    try s.push(32);
+    try s.push(33);
+    try s.push(34);
+    try s.push(35);
+    try s.push(36);
+    try s.push(37);
+
+    std.debug.print("capacity: {d}\n", .{s.capacity});
+
+    for(0..s.len) |i| {
+        std.debug.print("len {d}; {d} - {d}\n", .{s.len, i, s.pop()});
+    }
+
+    std.debug.print("len: {d}\n", .{s.len});
 
 }
 
