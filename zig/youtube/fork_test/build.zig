78901a47803c5f2b6379b158830a1c441cd7342e
MaximMGG <maxhrunenko@gmail.com>
2025-05-03 18:40:46 +0000

    fork test

---
+++ zig/youtube/fork_test/build.zig
@@ -0,0 +1,23 @@
+const std = @import("std");
+
+
+
+pub fn build(b: *std.Build) void {
+    const target = b.standardTargetOptions(.{});
+    const optimize = b.standardOptimizeOption(.{});
+
+
+    const exe = b.addExecutable(.{
+        .name = "fork",
+        .root_source_file = b.path("fork_test.zig"),
+        .optimize = optimize,
+        .target = target,
+    });
+
+
+    b.installArtifact(exe);
+    const run_cmd = b.addRunArtifact(exe);
+    const run_step = b.step("run", "run the application");
+
+    run_step.dependOn(&run_cmd.step);
+}
