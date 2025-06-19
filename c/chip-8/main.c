d0c8c20e5205ec6baa122a2c85e5555fa4bf6145
MaximMGG <maxhrunenko@gmail.com>
2025-06-19 19:40:11 +0000

    draw screen

--- c/chip-8/main.c
+++ c/chip-8/main.c
@@ -13,8 +13,19 @@ typedef struct {
     uint32_t  window_width;
     uint32_t fg_color;
     uint32_t bg_color;
+    uint32_t scale_factor;
 } config_t;
 
+typedef enum {
+    QUIT,
+    RUNNING,
+    PAUSED
+} emu_state;
+
+typedef  struct {
+    emu_state state;
+
+}chip8_t;
 
 bool init_sdl(sdl_t *sdl, const config_t config) {
     if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO | SDL_INIT_TIMER) != 0) {
@@ -24,7 +35,7 @@ bool init_sdl(sdl_t *sdl, const config_t config) {
 
     sdl->window = SDL_CreateWindow("CHIP8 Emulator", 
                     SDL_WINDOWPOS_CENTERED, 
-                    SDL_WINDOWPOS_CENTERED, config.window_width, config.window_height, 0);
+                    SDL_WINDOWPOS_CENTERED, config.window_width * config.scale_factor, config.window_height * config.scale_factor, 0);
 
     if (!sdl->window) {
         SDL_Log("Could not create SDL window %s\n", SDL_GetError());
@@ -41,8 +52,21 @@ bool init_sdl(sdl_t *sdl, const config_t config) {
     return true;
 }
 
+
+bool init_chip8(chip8_t *chip) {
+    chip->state = RUNNING;
+
+    return true;
+}
+
 bool set_config_from_args(config_t *config, const int argc, const char **argv) {
-    *config = (config_t){.window_width = 64, .window_height = 32, .fg_color = 0xFFFF00FF, .bg_color = 0x00000000};
+    *config = (config_t){
+            .window_width = 64, 
+            .window_height = 32, 
+            .fg_color = 0xFFFFFFFF, 
+            .bg_color = 0xFFFF00FF,
+            .scale_factor = 20,
+        };
 
     for(int i = 1; i < argc; i++) {
 
@@ -58,10 +82,10 @@ void final_cleanup(const sdl_t sdl) {
 }
 
 void clear_screen(const sdl_t *sdl, const config_t config) {
-    const uint8_t r = (uint8_t)config.bg_color >> 24 & 0xFF;
-    const uint8_t g = (uint8_t)config.bg_color >> 16 & 0xFF;
-    const uint8_t b = (uint8_t)config.bg_color >> 8 & 0xFF;
-    const uint8_t a = (uint8_t)config.bg_color & 0xFF;
+    const uint8_t r = (uint8_t)(config.bg_color >> 24);
+    const uint8_t g = (uint8_t)(config.bg_color >> 16);
+    const uint8_t b = (uint8_t)(config.bg_color >> 8);
+    const uint8_t a = (uint8_t)config.bg_color;
     SDL_SetRenderDrawColor(sdl->renderer, r, g, b, a);
     SDL_RenderClear(sdl->renderer);
 }
@@ -69,6 +93,27 @@ void clear_screen(const sdl_t *sdl, const config_t config) {
 void update_screen(sdl_t *sdl, config_t config) {
     SDL_RenderPresent(sdl->renderer);
 }
+
+void handle_input(chip8_t *chip8) {
+    SDL_Event ev;
+
+    while(SDL_PollEvent(&ev)) {
+        switch(ev.type) {
+            case SDL_QUIT: {
+                chip8->state = QUIT;
+                return;
+            } break;
+            case SDL_KEYDOWN: {
+
+            } break;
+            case SDL_KEYUP: {
+
+            } break;
+        }
+
+    }
+}
+
 
 int main(int argc, char **argv) {
     sdl_t sdl = {0};
@@ -82,9 +127,21 @@ int main(int argc, char **argv) {
         exit(EXIT_FAILURE);
     }
 
+    chip8_t chip8 = {0};
+    if (!init_chip8(&chip8)) {
+        exit(EXIT_FAILURE);
+    }
+
     clear_screen(&sdl, config);
 
-    while(true) {
+    while(chip8.state != QUIT) {
+
+        handle_input(&chip8);
+        if (chip8.state == PAUSED) {
+
+        }
+
+
         //Delay
         SDL_Delay(16);
 
