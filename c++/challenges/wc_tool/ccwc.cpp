0d3088cec436c783a19c96d41fb5e19cdc7aabb6
MaximMGG <maxhrunenko@gmail.com>
2025-07-11 20:50:05 +0300

    ccwc done, all functional work well

--- c++/challenges/wc_tool/ccwc.cpp
+++ c++/challenges/wc_tool/ccwc.cpp
@@ -20,14 +20,13 @@ int get_count_of_chars(const char *file_name) {
             char_count += l.size();
             char_count++;
         }
-        f.close();
         return char_count - 1;
     }
 
     return -1;
 }
 
-int get_count_of_words(std::ifstream& f) {
+int get_count_of_words(std::istream& f) {
     std::string s;
     int words_count{};
     while(std::getline(f, s)) {
@@ -45,7 +44,7 @@ int get_count_of_words(std::ifstream& f) {
     return words_count++;
 }
 
-int get_count_of_lines(std::ifstream& f) {
+int get_count_of_lines(std::istream& f) {
 
     int line_count{};
     char buf[512];
@@ -60,10 +59,12 @@ int get_count_of_lines(std::ifstream& f) {
     return line_count;
 }
 
-int get_count_of_bytes(std::ifstream& f) {
+int get_count_of_bytes(std::istream& f) {
 
     std::streampos file_end;
+    f.seekg(0, std::ios_base::end);
     file_end = f.tellg();
+    f.seekg(0, std::ios_base::beg);
 
     return int(file_end);
 }
@@ -101,14 +102,28 @@ int main(int argc, char **argv) {
             std::cout << std::setw(10) << get_count_of_chars(argv[2]) << " " << argv[2] << '\n';
         }
     } else {
-        std::ifstream f;
-        f.open(argv[1], std::ios_base::in);
-        std::cout << std::setw(10) << get_count_of_lines(f);
-        f.seekg(0, std::ios_base::beg);
-        std::cout << std::setw(10) << get_count_of_words(f);
-        f.seekg(0, std::ios_base::beg);
-        std::cout << std::setw(10) << get_count_of_bytes(f) << " " << argv[2] << '\n';
-        f.close();
+        if (argv[1][0] == '-') {
+            if (std::strcmp(argv[1], "-c") == 0) {
+                std::cout << std::setw(10) << get_count_of_bytes(std::cin) << '\n';
+            } else if (std::strcmp(argv[1], "-l") == 0) {
+                std::cout << std::setw(10) << get_count_of_lines(std::cin) << '\n';
+            } else if (std::strcmp(argv[1], "-w") == 0) {
+                std::cout << std::setw(10) << get_count_of_words(std::cin) << '\n';
+            }
+
+        } else {
+            std::ifstream f;
+            f.open(argv[1], std::ios_base::in);
+            std::cout << std::setw(10) << get_count_of_lines(f);
+            f.close();
+            f.open(argv[1], std::ios_base::in);
+            std::cout << std::setw(10) << get_count_of_words(f);
+            f.close();
+            f.open(argv[1], std::ios_base::in);
+            std::cout << std::setw(10) << get_count_of_bytes(f); 
+            std::cout << " " << argv[1] << std::endl;
+            f.close();
+        }
     }
 
     return 0;
