a96178f3e504347cc092dfeb7db7cdad2c4cce4c
MaximMGG <maxhrunenko@gmail.com>
2025-07-15 21:25:29 +0300

    implementing of http server in zig done

--- zig/introduction_in_zig/http_server/src/main.zig
+++ zig/introduction_in_zig/http_server/src/main.zig
@@ -1,5 +1,8 @@
 const std = @import("std");
 const sock_conf = @import("config.zig").Socket;
+const Request = @import("request.zig");
+const Response = @import("response.zig");
+const Method = Request.Method;
 
 const stdout = std.io.getStdOut().writer();
 
@@ -8,6 +11,19 @@ pub fn main() !void {
     try stdout.print("Server addres: {any}\n", .{socket._address});
     var server = try socket._address.listen(.{});
     const conn = try server.accept();
-    _ = conn;
+    defer conn.stream.close();
+
+    var buf: [1000]u8 = undefined;
+    @memset(&buf, 0);
+
+    try Request.read_request(conn, buf[0..buf.len]);
+    const request = Request.Request.parse_request(&buf);
+    if (request.method == Method.GET) {
+        if (std.mem.eql(u8, request.uri, "/")) {
+            try Response.send_200(conn);
+        } else {
+            try Response.send_404(conn);
+        }
+    }
 }
 
