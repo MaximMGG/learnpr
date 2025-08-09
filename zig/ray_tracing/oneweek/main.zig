0e296cdb41688fb6ae9f9cae74c6d022b2a00f07
MaximMGG <maxhrunenko@gmail.com>
2025-08-09 15:50:37 +0000

    zig performance increase

--- zig/ray_tracing/oneweek/main.zig
+++ zig/ray_tracing/oneweek/main.zig
@@ -3,35 +3,38 @@ const std = @import("std");
 const stdout = std.io.getStdOut().writer();
 const errout = std.io.getStdErr().writer();
 
+const BufWriter = std.io.BufferedWriter(4096, std.fs.File.Writer);
 
-fn write_color(handle: ) void {
+const Vec3 = @Vector(3, f64);
 
+fn write_color(writer: *BufWriter, v: Vec3) !void {
+    try writer.writer().print("{d} {d} {d}\n", .{
+        @as(i32, @intFromFloat(255.999 * v[0])),
+        @as(i32, @intFromFloat(255.999 * v[1])),
+        @as(i32, @intFromFloat(255.999 * v[2]))
+    });
 }
+
+
 
 pub fn main() !void {
     const image_width: i32 = 256;
     const image_height: i32 = 256;
 
-    std.io.getStdOut().handle;
-
     try stdout.print("P3\n{d} {d}\n255\n", .{image_width, image_height});
+    var bw = std.io.bufferedWriter(stdout);
+    defer bw.flush() catch {};
 
     for(0..image_height) |j| {
         try errout.print("\rScanlines remaining: {d}", .{image_height - j});
         for(0..image_width) |i| {
-            const r: f64 = @as(f64, @floatFromInt(i)) / (image_width - 1);
-            const g: f64 = @as(f64, @floatFromInt(j)) / (image_height - 1);
-            const b: f64 = 0.0;
-
-            const ir: i32 = @intFromFloat(255.999 * r);
-            const ig: i32 = @intFromFloat(255.999 * g);
-            const ib: i32 = @intFromFloat(255.999 * b);
-
-
-            try stdout.print("{d} {d} {d}\n", .{ir, ig, ib});
-
+            const vec = Vec3{
+                @as(f64, @floatFromInt(i)) / (image_width - 1),
+                @as(f64, @floatFromInt(j)) / (image_height - 1),
+                0.0
+            };
+            try write_color(&bw, vec);
+            //std.debug.print("{any}\n", .{@typeInfo(@TypeOf(writer))});
         }
     }
-
-
 }
