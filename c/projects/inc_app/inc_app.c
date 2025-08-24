3b99ef525bb764b71a653c7d1c941dad490bc6d1
MaximMGG <maxhrunenko@gmail.com>
2025-08-24 12:14:51 +0300

    inc_app read write expands struct and show info menu

--- c/projects/inc_app/inc_app.c
+++ c/projects/inc_app/inc_app.c
@@ -24,7 +24,7 @@ typedef struct {
 
 void check_inc_log() {
     log(INFO, "Check ing log");
-    str user_name = getenv("USERNAME");
+    str user_name = getenv("USER");
     snprintf(PATH_TO_INC_LOG, 128, PATH_TO_INC_LOG_FMT, user_name);
 
     if (access(PATH_TO_INC_LOG, F_OK)) {
@@ -33,17 +33,26 @@ void check_inc_log() {
         inc_fd = open(PATH_TO_INC_LOG, O_CREAT | O_RDWR | O_APPEND, S_IWUSR | S_IRUSR | S_IWGRP | S_IRGRP | S_IWOTH | S_IROTH);
         if (inc_fd < 0) {
             log(ERROR, "Cant open/create file %s", PATH_TO_INC_LOG);
+            exit(1);
         }
     } else {
         inc_fd = open(PATH_TO_INC_LOG, O_RDWR | O_APPEND, S_IWUSR | S_IRUSR | S_IWGRP | S_IRGRP | S_IWOTH | S_IROTH);
         if (inc_fd < 0) {
             log(ERROR, "Cant open/create file %s", PATH_TO_INC_LOG);
+            exit(1);
         }
     }
 }
 
-void cleanup() {
+void cleanup(list *l) {
     close(inc_fd);
+
+    for(i32 i = 0; i < l->len; i++) {
+        expends_t *tmp = list_get(l, i);
+        dealloc(tmp);
+    }
+    list_destroy(l);
+    log(INFO, "Cleanup");
 }
 
 list *read_inc_file() {
@@ -56,12 +65,87 @@ list *read_inc_file() {
     io[0].iov_len = sizeof(e);
 
     while(readv(inc_fd, io, 1) == sizeof(e)) {
-
-
+        expends_t *new = alloc_copy(&e, sizeof(expends_t));
+        list_append(l, new);
     }
 
+    return l;
 }
 
+
+void write_inc_file(list *l) {
+
+    struct iovec io[1] = {0};
+
+    for(i32 i = 0; i < l->len; i++) {
+        expends_t *tmp = list_get(l, i);
+        io[0].iov_base = tmp;
+        io[0].iov_len = sizeof(expends_t);
+        writev(inc_fd, io, 1);
+    }
+}
+
+
+void show_help() {
+    log(INFO, "Showing help");
+    WINDOW *help_window = newwin(LINES / 2, COLS / 2, LINES / 4, COLS / 4);
+    box(help_window, 0, 0);
+
+    mvwprintw(help_window, 1, 1, "Pres q/F1 for Quit");
+    mvwprintw(help_window, 2, 1, "Pres j for navigate down");
+    mvwprintw(help_window, 3, 1, "Pres k for navigate up");
+    mvwprintw(help_window, 4, 1, "Pres c for create new expands");
+    mvwprintw(help_window, 5, 1, "Pres d for delete expands");
+    mvwprintw(help_window, 6, 1, "Pres l for set limit");
+    mvwprintw(help_window, 7, 1, "Pres i for increase current expands");
+    mvwprintw(help_window, 8, 1, "Pres a to extract to file");
+    wrefresh(help_window);
+
+    wgetch(help_window);
+    wborder(help_window, ' ',' ',' ',' ',' ',' ',' ',' ');
+    wrefresh(help_window);
+    delwin(help_window);
+}
+
+
+str read_input(str input_name) {
+
+    WINDOW *input = newwin(3, COLS / 1.5, LINES - LINES / 8, COLS / 8);
+    box(input, 0, 0);
+    mvwprintw(input, 0, (COLS / 1.5) / 2, "%s", input_name);
+    wrefresh(input);
+    noecho();
+    i32 ch;
+    byte in[33] = {0};
+    i32 len = 0;
+    wmove(input, 1, len + 1);
+
+    while(ch != '\n') {
+        mvwprintw(input, 1, 1, "%s", in);
+        if (len == 32) {
+            break;
+        }
+        if (ch == 127) {
+            if (len > 0) {
+                len--;
+            }
+            in[len - 1] = ' ';
+            continue;
+        }
+        in[len] = ch;
+        len++;
+
+        wmove(input, 1, len + 1);
+        ch = wgetch(input);
+        log(INFO, "CH from input %d", ch);
+    }
+
+    wborder(input, ' ',' ',' ',' ',' ',' ',' ',' ');
+    wrefresh(input);
+    delwin(input);
+
+    return alloc_copy(in, len + 1);
+}
 
 
 int main() {
@@ -72,9 +156,49 @@ int main() {
     raw();
     keypad(stdscr, true);
     start_color();
+    refresh();
     check_inc_log();
+    i32 ch = 0;
+    i32 line = 0;
+
+    list *l = read_inc_file();
+
+    while(ch != KEY_F(1) && ch != 'q') {
+        mvprintw(0, 0, "%-30s %-30s %-30s %-30s", def_msg[0], def_msg[1], def_msg[2], def_msg[3]);
+
+
+
+        switch(ch) {
+            case 'j': {
+                if (line < l->len - 1) {
+                    line++;
+                }
+
+            } break;
+            case 'k': {
+                if (line > 0) {
+                    line--;
+                }
+            } break;
+            case 'h': {
+                show_help();
+                refresh();
+            } break;
+            case 'i': {
+                str r = read_input("test");
+                dealloc(r);
+            } break;
+
+        }
+
+        refresh();
+        ch = getch();
+        log(INFO, "ch == %c", ch);
+        clear();
+    }
 
     endwin();
+    cleanup(l);
     log(INFO, "Finish app work");
 
     return 0;
