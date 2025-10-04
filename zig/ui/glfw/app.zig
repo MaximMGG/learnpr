95b261b9e5ecf429bc1b2e6bf22d1896b8c478ce
MaximMGG <maxhrunenko@gmail.com>
2025-10-04 11:31:45 +0300

    glfw in zig

--- zig/ui/glfw/app.zig
+++ zig/ui/glfw/app.zig
@@ -1,7 +1,8 @@
 const std = @import("std");
+const c = @cImport(@cInclude("stdio.h"));
 const glew = @cImport(@cInclude("GL/glew.h"));
 const gl = @cImport(@cInclude("GL/gl.h"));
-const glfw = @cImport(@cInclude({"GLFW/glfw3.h";}));
+const glfw = @cImport(@cInclude("GLFW/glfw3.h"));
 
 
 const WIDTH = 640;
@@ -21,9 +22,35 @@ pub fn main() !void {
     glfw.glfwWindowHint(glfw.GLFW_CONTEXT_VERSION_MINOR, 3);
     glfw.glfwWindowHint(glfw.GLFW_OPENGL_PROFILE, glfw.GLFW_OPENGL_CORE_PROFILE);
 
-    const window: ?*glfw.GLFWwindow = glfw.glfwCreateWindow(WIDTH, HEIGHT, "TEST WINDOW", null, null);
+    const window: *glfw.GLFWwindow = glfw.glfwCreateWindow(WIDTH, HEIGHT, "TEST WINDOW", null, null) orelse {
+        std.debug.print("GLFWwindow in null", .{});
+        return;
+    }; 
+
+    std.debug.print("Ceate window\n", .{});
+
+    glfw.glfwMakeContextCurrent(window);
+
+    if (glew.glewInit() != glew.GLEW_OK) {
+        std.debug.print("glewInit fail\n", .{});
+        glfw.glfwDestroyWindow(window);
+        glfw.glfwTerminate();
+        return;
+    }
+
+    std.debug.print("glewInit\n", .{});
+    glfw.glfwSwapInterval(1);
+
+    std.debug.print("GL Version: {s}\n", .{gl.glGetString(gl.GL_VERSION)});
+
+    while(glfw.glfwWindowShouldClose(window) == 0) {
+        gl.glClear(gl.GL_COLOR_BUFFER_BIT);
 
 
+        glfw.glfwSwapBuffers(window);
+        glfw.glfwPollEvents();
+    }
 
+    glfw.glfwDestroyWindow(window);
     glfw.glfwTerminate();
 }
