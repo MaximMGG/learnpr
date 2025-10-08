2394dfbeae55d4ced919bc8328b189de67d1ed76
MaximMGG <maxhrunenko@gmail.com>
2025-10-08 20:52:25 +0300

    draw sqare

--- c/glfw/hometask/drow_triangle.c
+++ c/glfw/hometask/drow_triangle.c
@@ -5,6 +5,7 @@
 #include <cstdext/core.h>
 #include <cstdext/io/logger.h>
 #include <cstdext/io/reader.h>
+#include <unistd.h>
 
 #define WIDTH 720
 #define HEIGHT 480
@@ -47,12 +48,13 @@ int main() {
 
 
     f32 pos[] = {
-        -0.5f, -0.5f, 0.0f,
-         0.5f, -0.5f, 0.0f,
-         0.0f,  0.5f, 0.0f
+        0.5f,  0.5f,  // top right
+        0.5f, -0.5f,  // bottom right
+       -0.5f,  0.5f,  // top left 
+       -0.5f, -0.5f,  // bottom left
     };
 
-    u32 index[] = {0, 1, 2};
+    u32 index[] = {0, 1, 2, 2, 3, 1};
 
     u32 VBO; // vertex buffer objects
     glGenBuffers(1, &VBO);
@@ -66,7 +68,14 @@ int main() {
     log(INFO, "Bind vertex arrats");
 
 
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void *)0);
+    u32 EBO;// elemet buffer array object
+
+    glGenBuffers(1, &EBO);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(index), index, GL_STATIC_DRAW);
+
+
+    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), (void *)0);
     glEnableVertexAttribArray(0);
     log(INFO, "gl attrib pointer done");
 
@@ -119,9 +128,15 @@ int main() {
         dealloc(err_buf);
         goto fatal_error;
     }
+    f32 r = 0.8f;
+
+
     glUseProgram(program);
+
+    glUniform4f(glGetUniformLocation(program, "u_Color"), r, 0.2f, 0.4f, 1.0f);
     glDeleteShader(vertexShader);
     glDeleteShader(fragmentShader);
+
 
     log(INFO, "Use pogram");
     dealloc(vertexShaderSource);
@@ -129,6 +144,7 @@ int main() {
 
 
     log(INFO, "Start cycle");
+    f32 inc = 0.05f;
 
     while(!glfwWindowShouldClose(window)) {
         //set standard OpenGL backgroud color
@@ -136,12 +152,26 @@ int main() {
         glClear(GL_COLOR_BUFFER_BIT);
 
         glUseProgram(program);
+
+        glUniform4f(glGetUniformLocation(program, "u_Color"), r, 0.2f, 0.4f, 1.0f);
+        if (r > 1.0f) {
+            inc = -0.05f;
+        } else if (r < 0.0f){
+            inc = 0.05f;
+        }
+
+        r += inc;
+
+
         glBindVertexArray(VAO);
-        glDrawArrays(GL_TRIANGLES, 0, 3);
+        //glDrawArrays(GL_TRIANGLES, 0, 3);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
 
         glfwSwapBuffers(window);
         glfwPollEvents();
+
+        usleep(40000);
     }
 
     log(INFO, "End cycle");
