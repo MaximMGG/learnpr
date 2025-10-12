4f123693f4f89536712d0045d077fd65da9a9332
MaximMGG <maxhrunenko@gmail.com>
2025-10-12 10:37:10 +0300

    haffman encoding in process

--- zig/challenges/compres/compres.zig
+++ zig/challenges/compres/compres.zig
@@ -1,5 +1,76 @@
 const std = @import("std");
 
+pub fn sort_lists(chars: *std.ArrayList(u8), count: *std.ArrayList(u32)) void {
+    var start: usize = 0;
+    const end: usize = chars.items.len;
+    var char_slice = chars.items[start..end];
+    var count_slice = count.items[start..end];
+
+    while(char_slice.len > 1) {
+        var min: u32 = 1 << 31;
+        var index: usize = 0;
+        for(count_slice, 0..) |weight, i| {
+            if (weight < min) {
+                min = weight;
+                index = i;
+            }
+        }
+
+        const tmp_count: u32 = count_slice[0];
+        const tmp_char: u8 = char_slice[0];
+        count_slice[0] = count_slice[index];
+        char_slice[0] = char_slice[index];
+        count_slice[index] = tmp_count;
+        char_slice[index] = tmp_char;
+        start += 1;
+        count_slice = count.items[start..end];
+        char_slice = chars.items[start..end];
+    }
+}
+
+const Node = struct {
+    weight: u32,
+    val:    u8,
+    leaf:   bool,
+    left:   ?*Node = null,
+    right:  ?*Node = null,
+};
+
+pub fn buildHuffmanTree(chars: *std.ArrayList(u8), count: *std.ArrayList(u32)) Node {
+    var cur_node: Node = undefined;
+    cur_node.left.?.* = Node{.weight = count.items[0], .val = chars.items[0], .leaf = true};
+    cur_node.right.?.* = Node{.weight = count.items[1], .val = chars.items[1], .leaf = true};
+    cur_node.leaf = false;
+    cur_node.weight = cur_node.left.?.weight + cur_node.right.?.weight;
+    cur_node.val = 0;
+
+    var i: usize = 2;
+
+    while(i < chars.items.len) {
+        var left: Node = undefined;
+        var right: Node = undefined;
+
+        if (cur_node.weight < count.items[i]) {
+            left = cur_node;
+            right = .{.weight = count.items[i], .val = chars.items[i], .leaf = true};
+
+        } else {
+            right = cur_node;
+            left = .{.weight = count.items[i], .val = chars.items[i], .leaf = true};
+        }
+
+        cur_node.weight = left.weight + right.weight;
+        cur_node.leaf = false;
+        cur_node.left.?.* = left;
+        cur_node.right.?.* = right;
+
+        i += 1;
+    }
+
+    return cur_node;
+}
+
+
 fn contain_in_list(list: *std.ArrayList(u8), c: u8) ?usize {
     for(list.items, 0..) |e, i| {
         if (e == c) {
@@ -9,9 +80,12 @@ fn contain_in_list(list: *std.ArrayList(u8), c: u8) ?usize {
     return null;
 }
 
+
+
+
 pub fn main() !void {
     const allocator = std.heap.page_allocator;
-    const test_file = try std.fs.cwd().openFile("test.txt", .{.mode = .read_only});
+    const test_file = try std.fs.cwd().openFile("test2.txt", .{.mode = .read_only});
     defer test_file.close();
     const file_stat = try test_file.stat();
     var file_buf: [4096]u8 = undefined;
@@ -41,10 +115,8 @@ pub fn main() !void {
         }
     }
 
-    var total: u64 = 0;
-    for(chars.items, count.items) |c, i| {
-        std.debug.print("Char: {d} - {d}\n", .{@as(u32, @intCast(c)), i});
-        total += i;
-    }
-    std.debug.print("Total chars: {d}\n", .{total});
+    sort_lists(&chars, &count);
+    const node = buildHuffmanTree(&chars, &count);
+
+    std.debug.print("{any}\n", .{node});
 }
