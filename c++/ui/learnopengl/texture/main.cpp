038104959ea40742abc1b7bb8aea325dcac6198d
MaximMGG <maxhrunenko@gmail.com>
2025-10-20 22:19:40 +0300

    c++ learnopenGL texture loadiong

---
+++ c++/ui/learnopengl/texture/main.cpp
@@ -0,0 +1,120 @@
+#include <iostream>
+#include <GL/glew.h>
+#include <GL/gl.h>
+#include <GLFW/glfw3.h>
+#include "shader.hpp"
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
+
+
+#define WIDTH 1280
+#define HEIGHT 720
+
+int main() {
+
+    std::cout << "Start application\n";
+    glfwInit();
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 5);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    GLFWwindow *window = glfwCreateWindow(WIDTH, HEIGHT, "Texture loading", nullptr, nullptr);
+
+    if (window == NULL) {
+        std::cerr << "glfwCreateWindow failed\n";
+        return 1;
+    }
+
+    glfwMakeContextCurrent(window);
+
+    if (glewInit() != 0) {
+        std::cout << "glewInit failed\n";
+        return 1;
+    }
+
+    Shader shader("vertex.glsl", "fragment.glsl");
+    std::cout << "shader compile\n";
+
+    float vertices[] = {
+         0.5f,  0.5f, 0.0f,   1.0f, 0.0f, 0.0f,   1.0f, 1.0f, 
+         0.5f, -0.5f, 0.0f,   0.0f, 1.0f, 0.0f,   1.0f, 0.0f,
+        -0.5f, -0.5f, 0.0f,   0.0f, 0.0f, 1.0f,   0.0f, 0.0f,
+        -0.5f,  0.5f, 0.0f,   1.0f, 1.0f, 0.0f,   0.0f, 1.0f
+    };
+
+    unsigned int indices[] = {
+        0, 1, 3, 1, 2, 3
+    };
+
+    unsigned int VBO, VAO, EBO;
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &VBO);
+    glGenBuffers(1, &EBO);
+
+    glBindVertexArray(VAO);
+
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void *)0);
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void *)(3 * sizeof(float)));
+    glEnableVertexAttribArray(1);
+    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void *)(6 * sizeof(float)));
+    glEnableVertexAttribArray(2);
+
+    std::cout << "Set attrib poiner\n";
+
+
+    unsigned int texture;
+    glGenTextures(1, &texture);
+    glBindTexture(GL_TEXTURE_2D, texture);
+
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+    int width, height, nrChannels;
+
+    unsigned char *data = stbi_load("./wall.jpg", &width, &height, &nrChannels, 0);
+    if (data) {
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+        glGenerateMipmap(GL_TEXTURE_2D);
+    } else {
+        std::cout << "Cant load image\n";
+    }
+
+    stbi_image_free(data);
+
+    std::cout << "Start main loop\n";
+
+    while(!glfwWindowShouldClose(window)) {
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glBindTexture(GL_TEXTURE_2D, texture);
+
+        shader.use();
+        glBindVertexArray(VAO);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+
+    glDeleteVertexArrays(1, &VAO);
+    glDeleteBuffers(1, &VBO);
+    glDeleteBuffers(1, &EBO);
+
+
+    glfwTerminate();
+    return 0;
+}
---
+++ c++/ui/learnopengl/texture/shader.hpp
@@ -0,0 +1,92 @@
+#ifndef SHADER_H
+#define SHADER_H
+
+#include <GL/glew.h>
+#include <fstream>
+#include <iostream>
+#include <sstream>
+#include <string>
+
+struct Shader {
+    unsigned int ID = 0;
+
+    Shader(const char *vertexPath, const char *fragmentPath) {
+        std::ifstream vertexFile(vertexPath);
+        std::ifstream fragmentFile(fragmentPath);
+        std::string vertexCode;
+        std::string fragmentCode;
+
+        std::stringstream vertexStream, fragmentStream;
+        vertexStream << vertexFile.rdbuf();
+        fragmentStream << fragmentFile.rdbuf();
+
+        vertexCode = vertexStream.str();
+        fragmentCode = fragmentStream.str();
+
+        unsigned int vertex, fragment;
+
+        vertex = glCreateShader(GL_VERTEX_SHADER);
+        fragment = glCreateShader(GL_FRAGMENT_SHADER);
+
+        const char *vShaderCode = vertexCode.c_str();
+        const char *fShaderCode = fragmentCode.c_str();
+
+        glShaderSource(vertex, 1, &vShaderCode, NULL);
+        glCompileShader(vertex);
+        if (!checkCompileError(vertex, "VERTEX")) {
+            glDeleteShader(vertex);
+            return;
+        }
+        glShaderSource(fragment, 1, &fShaderCode, NULL);
+        glCompileShader(fragment);
+        if (!checkCompileError(fragment, "FRAGMENT")) {
+            glDeleteShader(fragment);
+            return;
+        }
+        unsigned int program = glCreateProgram();
+
+        glAttachShader(program, vertex);
+        glAttachShader(program, fragment);
+        glLinkProgram(program);
+        if (checkCompileError(program, "PROGRAM")) {
+            glDeleteProgram(program);
+            return;
+        }
+
+        glDeleteShader(vertex);
+        glDeleteShader(fragment);
+    }
+    
+    ~Shader() {
+        glDeleteProgram(ID);
+    }
+
+    void use() {
+        glUseProgram(ID);
+    }
+
+
+    bool checkCompileError(unsigned int shader, const char *type) {
+        int res;
+        char infolog[1024]{};
+
+        if (type[0] == 'P' || type[0] == 'p') {
+            glGetProgramiv(shader, GL_LINK_STATUS, &res);
+            if (!res) {
+                glGetProgramInfoLog(shader, 1024, NULL, infolog);
+                std::cerr << "Program linking error: " << infolog << '\n';
+                return false;
+            }
+        } else {
+            glGetShaderiv(shader, GL_COMPILE_STATUS, &res);
+            if (res == GL_FALSE) {
+                glGetShaderInfoLog(shader, 1024, NULL, infolog);
+                std::cerr << "Shader compilation error: " << type << " : " << infolog << '\n';
+                return false;
+            }
+        }
+        return true;
+    }
+};
+
+#endif
