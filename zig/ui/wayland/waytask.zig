780c4130ada4d083a6977eeff2ce9a82ac554629
MaximMGG <maxhrunenko@gmail.com>
2025-10-23 21:51:46 +0300

    wayland little try

--- zig/ui/wayland/waytask.zig
+++ zig/ui/wayland/waytask.zig
@@ -6,14 +6,22 @@ const w = @cImport({
 const pty = @cImport(@cInclude("pty.h"));
 
 
+const compositor: ?*w.wl_compositor = null;
+const shm: ?*w.wl_shm = null;
+const wm_base: ?*w.xdg_wm_base = null;
+
+
+const reglistener = w.wl_registry_listener{};
+
+
 pub fn main() void {
-    var wl_display = w.wl_display_connect(null) orelse {
+    const wl_display = w.wl_display_connect(null) orelse {
         @panic("Cant create wayland display");
     };
 
 
-    _ = &wl_display;
-
+    const registry = w.wl_display_get_registry(wl_display);
+    _ = w.wl_registry_add_listener(registry, null, null);
 
 
     std.debug.print("All done\n", .{});
