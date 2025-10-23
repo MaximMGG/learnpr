3a508f81f8d3d1040c7cd94f068acf05ea54f82d
MaximMGG <maxhrunenko@gmail.com>
2025-10-23 22:33:30 +0300

    math openGL vrite shader struct

---
+++ c++/ui/learnopengl/math/main.cpp
@@ -0,0 +1,12 @@
+#include <GL/glew.h>
+#include <GL/gl.h>
+#include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+#include <iostream>
+
+
+int main() {
+
+}
