bc9a66353ebe306d39177e5df4bc0c54ea762390
MaximMGG <maxhrunenko@gmail.com>
2025-11-24 23:38:07 +0200

    try to nimlsp work, but not

--- nim/ui/glfw/test.nim
+++ nim/ui/glfw/test.nim
@@ -6,13 +6,20 @@ import std/sequtils
 
 proc compileShader(path: string, s_type: uint32): uint32 = 
     var file = readFile(path)
-    var buf = file.toSeq()
     var shader = glCreateShader(cast[GLenum](s_type))
-    var cbuf: cstringArray = UncheckedArray[cstring][addr buf[0]]
-    shader.glShaderSource(1, nil, nil)
+    var cbuf: cstringArray = cast[cstringArray](addr file)
+    shader.glShaderSource(1, cbuf, nil)
+    glCompileShader(shader)
+    return shader
 
 proc loadShader(v_path: string, f_path: string): uint32 =
-    return 0
+    var v_shader: uint32 = compileShader(v_path, cast[uint32](GL_VERTEX_SHADER))
+    var f_shader: uint32 = compileShader(f_path, cast[uint32](GL_FRAGMENT_SHADER))
+    var prog: uint32 = glCreateProgram()
+    glAttachShader(prog, v_shader)
+    glAttachShader(prog, f_shader)
+    glLinkProgram(prog)
+    return prog
 
 
 proc main() =
@@ -25,6 +32,7 @@ proc main() =
   glfw.makeContextCurrent(window)
   openGL.loadExtensions()
 
+  var prog: uint32 = loadShader("./vertex.glsl", "./fragment.glsl")
   var vertices = @[
     0.5, 0.5,
     0.5, -0.5,
@@ -44,11 +52,16 @@ proc main() =
   glGenBuffers(1, addr EBO)
   glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO)
   glBufferData(GL_ELEMENT_ARRAY_BUFFER, indeces.len * sizeof(uint32), addr indeces[0], GL_STATIC_DRAW)
-  
+
+  glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, )
+  glEnableVertexAttribArray(0)
 
   
   while not window.shouldClose():
     glClear(GL_COLOR_BUFFER_BIT)
+
+    glBindVertexArray(VAO);
+    glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nil)
 
     window.swapBuffers()
     glfw.pollEvents()
