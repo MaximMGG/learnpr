f881bfa7cc2b3a29e8613e7ff140882e21ebf546
MaximMGG <maxhrunenko@gmail.com>
2025-11-29 10:46:28 +0200

    init v project

---
+++ nim/test/network.nim
@@ -0,0 +1,13 @@
+import std/net
+
+
+
+proc connect_test() =
+  let socket = newSocket()
+  let ctx = newContext()
+  wrapSocket(ctx, socket)
+  socket.connect("google.com", Port(443))
+  
+
+
+connect_test()
