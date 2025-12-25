587b063ba870da63ba5cc011dbfa3c8195466897
MaximMGG <maxhrunenko@gmail.com>
2025-12-25 21:57:43 +0200

    database insert fix

--- zig/test/token_test/database_test.zig
+++ zig/test/token_test/database_test.zig
@@ -6,82 +6,37 @@ const Database = struct {
     conn: *db.PGconn,
     res: *db.PGresult,
 
-    dbname: []const u8,
-    user_name: []const u8,
-    user_password: []const u8,
 
-    allocator: std.mem.Allocator,
-};
 
-fn db_connect(allocator: std.mem.Allocator, dbname: []const u8, user_name: []const u8, user_password: []const u8) !?*Database {
-    const dbase = try allocator.create(Database);
-    const connect_string = try std.fmt.allocPrint(allocator, "dbname={s} user={s} password={s}", .{ dbname, user_name, user_password });
-    std.debug.print("{s}\n", .{connect_string});
-    defer allocator.free(connect_string);
-    dbase.conn = db.PQconnectdb(@ptrCast(connect_string.ptr)).?;
-    if (db.PQstatus(dbase.conn) != db.CONNECTION_OK) {
-        std.log.err("PQconnectdb error with conn_string: {s}\n", .{connect_string});
-        return null;
-    }
-
-    dbase.dbname = try std.fmt.allocPrint(allocator, "{s}", .{dbname});
-    dbase.user_name = try std.fmt.allocPrint(allocator, "{s}", .{user_name});
-    dbase.user_password = try std.fmt.allocPrint(allocator, "{s}", .{user_password});
-    dbase.allocator = allocator;
-
-    return dbase;
-}
-
-fn db_select(database: *Database, query: []const u8) !?[][][]u8 {
-    database.res = db.PQexec(database.conn, query.ptr).?;
-    if (db.PQresultStatus(database.res) != db.PGRES_TUPLES_OK) {
-        std.log.err("PQexec with query {s} error -> {s}\n", .{ query, db.PQerrorMessage(database.conn) });
-        return null;
-    }
-    const rows = db.PQntuples(database.res);
-    const cols = db.PQnfields(database.res);
-    const res = try database.allocator.alloc([][]u8, @intCast(rows));
-
-    for (0.., res) |i, *row| {
-        row.* = try database.allocator.alloc([]u8, @intCast(cols));
-        for (0.., row.*) |j, *c| {
-            c.* = try database.allocator.dupe(u8, std.mem.span(db.PQgetvalue(database.res, @intCast(i), @intCast(j)).?));
+    fn connect(allocator: std.mem.Allocator, dbname: []const u8, user: []const u8, password: []const u8) !*Database {
+        const dbase = try allocator.create(Database);
+        const connstr = try std.fmt.allocPrint(allocator, "dbname={s} user={s} password={s}", .{dbname, user, password});
+        
+        dbase.conn = db.PQconnectdb(connstr.ptr).?;
+        if (db.PQstatus(dbase.conn) != db.CONNECTION_OK) {
+            std.debug.print("Connection dab\n", .{});
+            return error.BadConnection;
         }
+        return dbase;
     }
-    return res;
-}
 
-fn db_disconect(dbase: *Database) void {
-    dbase.allocator.free(dbase.dbname);
-    dbase.allocator.free(dbase.user_name);
-    dbase.allocator.free(dbase.user_password);
-    db.PQfinish(dbase.conn);
-    dbase.allocator.destroy(dbase);
-}
+    fn insert(self: *Database, query: []const u8) void {
+        self.res = db.PQexec(self.conn, query.ptr).?;
+        if (db.PQresultStatus(self.res) != db.PGRES_COMMAND_OK) {
+           const err = std.mem.span(db.PQerrorMessage(self.conn)); 
+           std.debug.print("Error: {s}\n", .{err});
+        }
+        db.PQclear(self.res);
+    }
+   
+    fn disconnect(self: *Database) void {
+        db.PQfinish(self.conn);
+    }
+};
 
 pub fn main() !void {
     const allocator = std.heap.page_allocator;
-    const conn = try db_connect(allocator, "mydb", "maxim", "maxim") orelse return;
-    defer db_disconect(conn);
-    std.debug.print("User: {s}\n", .{conn.user_name});
-
-    const res = try db_select(conn, "select * from token");
-
-    for (res.?) |tuple| {
-        for (tuple) |val| {
-            std.debug.print("{s}\n", .{val});
-        }
-    }
-}
-
-
-test "insert into db test" {
-    const allocator = testing.allocator;
-    var dbase = (try db_connect(allocator, "mydb", "maxim", "maxim")).?;
-    db_disconect(dbase);
-    dbase.res = db.PQexec(dbase.conn, "INSET INTO test_table(id, value, price) VALUES(11, 123, 777,222)").?;
-    if (db.PQresultStatus(dbase.res) != db.PGRES_COMMAND_OK) {
-        const err = std.mem.span(db.PQerrorMessage(dbase.conn));
-        std.debug.print("Error: {s}\n", .{err});
-    }
+    const dbase = try Database.connect(allocator, "mydb", "maxim", "maxim");
+    defer dbase.disconnect();
+    dbase.insert("INSERT INTO test_table(id, value, price) VALUES(112, 888, 1384.888)");
 }
