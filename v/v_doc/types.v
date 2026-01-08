7f0dab271a4af827bd0bc4d36aeaf4afc0a80d18
MaximMGG <maxhrunenko@gmail.com>
2026-01-08 21:52:28 +0200

    arrays methods and slice operations

--- v/v_doc/types.v
+++ v/v_doc/types.v
@@ -1,6 +1,12 @@
+
 struct User {
 	x int
 	y f64
+}
+
+struct SuperUser {
+  age int
+  name string
 }
 
 fn formatting() {
@@ -32,6 +38,29 @@ fn formatting() {
 	println('${'abd':3R}')
 }
 
+fn multy_array() {
+  mut a := [][]int{len: 2, init: []int{len: 3}}
+  for i in 0 .. a.len {
+    for j in 0 .. a[i].len {
+      a[i][j] = j * i + i
+    }
+  }
+  println("${a}")
+
+  mut b := [][][]int{}
+  mut index := i32(0)
+
+  for mut i in b {
+    for mut j in i {
+      for mut k in j {
+        k = index
+         index++
+      }
+    }
+  }
+  println("${b}")
+}
+
 fn string_operations() {
 	name := 'bob'
 	bobby := name + 'by'
@@ -45,6 +74,8 @@ fn string_operations() {
 	println('age = ${age}') // this is good
 }
 
+
+
 fn arrays() {
 	mut num := [1, 2, 3]
 	num << 88
@@ -56,30 +87,90 @@ fn arrays() {
   println("${arr}")
 }
 
+fn arr_methods() {
+  arr := [1, 2, 3, 4, 5, 6, 7]
 
-fn multy_array() {
-  mut a := [][]int{len: 2, init: []int{len: 3}}
-  for i in 0 .. a.len {
-    for j in 0 .. a[i].len {
-      a[i][j] = j * i + i
-    }
-  }
-  println("${a}")
+  arr2 := arr.filter(it % 2 == 0)
+  println("before fileter -> ${arr}\nAfter filter -> ${arr2}")
 
-  mut b := [][][]int{len: 2, init: [][]int{len: 3, init: []int{len: 3, init: 0}}}
-  mut index := i32(0)
 
-  for mut i in b {
-    for mut j in i {
-      for mut k in j {
-        k = index
-        index++
+  words := ['hello', 'world']
+  upper := words.map(it.to_upper())
+  println("Before mapping -> ${words}\nAfter mapping -> ${upper}")
+  
+  mut for_sort := [12, 3, 5, 9, 3, 1, 8, 4, 5, 6, 1, 2, 9, 78, 4245, 234, 999, 0]
+  println("Before sorting -> ${for_sort}")
+  for_sort.sort(a < b)
+  println("After sorting -> ${for_sort}")
+
+  mut users := [SuperUser{21, 'Bob'}, SuperUser{33, 'Mulkya'}, SuperUser{10, "Ashly"}]
+  println("Users ${users}")
+  users.sort(a.age < b.age)
+  println("Sort by age: ${users}")
+  users.sort(a.name > b.name)
+  println("Sort by name: ${users}")
+
+  custom_sort_fn := fn (a &SuperUser, b &SuperUser) int {
+    if a.name == b.name {
+      if a.age < b.age {
+        return 1
       }
+      if a.age > b.age {
+        return -1
+      }
+      return 0
     }
+    if a.name < b.name {
+      return -1
+    } else if a.name > b.name {
+      return 1
+    }
+    return 0
   }
-  println("${b}")
+
+
+  users.sort_with_compare(custom_sort_fn)
+  println("After custom sort fn -> ${users}")
+  
 }
 
+fn safe_slice() {
+  mut a := [0, 1, 2, 3, 4, 5, 6, 7]
+
+  mut b := a[2..4]
+  b[0] = 7
+  println(a)
+  b << 9
+  println(a)
+  println(b)
+
+}
+
+fn unsafe_slice() {
+  mut a := [0, 1, 2, 3, 4, 5, 6, 7]
+
+  mut b := unsafe { a[2..4]}
+  b[0] = 7
+  println(a)
+  b << 9
+  println(a)
+  println(b)
+}
+
+fn unsafe_slice2() {
+  mut a := []int{len: 5, cap: 6, init: 2}
+  mut b := unsafe {a[1..4]}
+
+  a << 3
+  b[2] = 13
+
+  a << 4
+
+  b[1] = 99
+  println(a)
+  println(b)
+
+}
 
 fn main() {
 	s := 'hello'
@@ -103,4 +194,13 @@ fn main() {
 	arrays()
   println("Multy arrays")
   multy_array()
+  println("Array methods")
+  arr_methods()
+  println("Unsafe slice")
+  unsafe_slice()
+  println("Safe slice")
+  safe_slice()
+  println("Unsafe slice 2")
+  unsafe_slice2()
+
 }
