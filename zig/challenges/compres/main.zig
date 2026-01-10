c3556c8842b2a8daf0c7288a0eb3a5988e92b578
MaximMGG <maxhrunenko@gmail.com>
2026-01-10 23:33:10 +0200

    huffman compression in zig, build map and priority queue, build huffman tree

--- zig/challenges/compres/main.zig
+++ zig/challenges/compres/main.zig
@@ -1,44 +1,84 @@
 const std = @import("std");
 
-const FILE = "test.txt";
 
-pub fn main() !void {
+fn build_frequensy(allocator: std.mem.Allocator, file: []u8) !std.AutoHashMap(u8, u32) {
+    var m = std.AutoHashMap(u8, u32).init(allocator);
 
-    const allocator = std.heap.page_allocator;
-
-    var tree = std.AutoHashMap(u8, i32).init(allocator);
-    defer tree.deinit();
-    var buf: [4096]u8 = undefined;
-
-    var stdout_writer = std.fs.File.stdout().writer(&buf);
-    var stdout = &stdout_writer.interface;
-
-
-    const file = try std.fs.cwd().openFile(FILE, .{.mode = .read_only});
-    defer file.close();
-    const file_stat = try file.stat();
-    var file_reader = file.reader(&buf);
-    const reader = &file_reader.interface;
-
-    const file_buf = try reader.readAlloc(allocator, file_stat.size);
-    defer allocator.free(file_buf);
-
-    var total: u64 = 0;
-    for(0..file_buf.len) |i| {
-        if (tree.getEntry(file_buf[i])) |entry| {
+    for (file) |c| {
+        if (m.contains(c)) {
+            const entry = m.getEntry(c).?;
             entry.value_ptr.* += 1;
         } else {
-            try tree.put(file_buf[i], 1);
+            try m.put(c, 1);
         }
-        total += 1;
     }
 
-    var it = tree.iterator();
+    return m;
+}
 
+const Node = struct {
+    freq: u32,
+    letter: u8,
+    is_liaf: bool,
+    left: ?*Node = null,
+    right: ?*Node = null,
+};
+
+fn less(context: void, a: Node, b: Node) std.math.Order {
+    _ = context;
+    if (a.freq == b.freq) {
+        return std.math.order(a.letter, b.letter);
+    }
+    return std.math.order(a.freq, b.freq);
+}
+
+
+fn build_priority_queue(allocator: std.mem.Allocator, m: std.AutoHashMap(u8, u32)) !std.PriorityQueue(*Node) {
+    var pq = std.PriorityQueue(*Node, void, less).init(allocator);
+
+    var it = m.iterator();
     while(it.next()) |entry| {
-        try stdout.print("Char: {c} - Value: {d}\n", .{entry.key_ptr.*, entry.value_ptr.*});
+        const n = try allocator.create(Node);
+        n.letter = entry.key_ptr.*;
+        n.freq = entry.value_ptr.*;
+        n.is_liaf = true;
+        try pq.add(n);
     }
-    try stdout.print("Total is: {d}\n", .{total});
 
-    try stdout.flush();
+    return pq;
+}
+
+fn build_huffman_tree(allocator: std.mem.Allocator, pq: *std.PriorityQueue(*Node)) !*Node {
+    while(pq.count() > 1) {
+        const l = pq.remove();
+        const r = pq.remove();
+        const new_node = try allocator.create(Node);
+        new_node.is_liaf = false;
+        new_node.freq = l.freq + r.freq;
+        new_node.left = l;
+        new_node.right = r;
+        try pq.add(new_node);
+    }
+}
+
+pub fn main() !void {
+    const allocator = std.heap.page_allocator;
+    const args = try std.process.argsAlloc(allocator);
+    if (args.len < 2) {
+        std.debug.print("Usage: compress [file_name]\n", .{});
+        return;
+    }
+
+    const file = try std.fs.cwd().openFile(args[1], .{.mode = .read_only});
+    defer file.close();
+    const stat = try file.stat();
+
+    const buf = try allocator.alloc(u8, stat.size);
+    _ = try file.read(buf);
+
+    var m = try build_frequensy(allocator, buf);
+    defer m.deinit();
+
+    var pq = try build_priority_queue(allocator, m);
+
 }
