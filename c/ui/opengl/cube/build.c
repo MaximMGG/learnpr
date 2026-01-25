8734fdd50fa76b9fcd28314e82eab9707e960950
MaximMGG <maxhrunenko@gmail.com>
2026-01-25 18:51:50 +0200

    cbuild test with openGL app

--- c/ui/opengl/cube/build.c
+++ c/ui/opengl/cube/build.c
@@ -1,9 +1,12 @@
 #include <cstdext/build.h>
 
-void build(Args args) {
+void build(Args *args) {
   Exe *e = buildCreateExe(heap_allocator, "cube", "main.c");
   buildExeAddSrc(e, "main.c");
   buildExeLinkLocallib(e, "../lib/", "util");
   buildExeLinkSyslib(e, "GL", "GLEW", "glfw", "cext", "m");
   buildExeCompile(e);
+  if (buildIsArg(args, "run")) {
+    buildExeRun(e);
+  }
 }
