07b0f8f910aef44c0f4139cdf5c1f13d8c456191
MaximMGG <maxhrunenko@gmail.com>
2026-04-05 16:32:07 +0300

    module 3 done

--- rust/learnrust/module_3/src/main.rs
+++ rust/learnrust/module_3/src/main.rs
@@ -31,13 +31,26 @@ fn main() {
     _ = b;
 
     do_stuf();
+	nasted_asigment();
+	let y = get_num(123);
+	println!("Get num is: {y}");
 
+	let res = cond_res(false);
+	println!("Cond res is: {res}");
+
+	println!("Result from loop: {}", from_loop(12));
+	loop_lables();
+	in_element_for();
+	for_range();
+	println!("Fibonache of 7: {}", fibonache(14));
 }
 
 fn do_stuf() {
-
     let arr = [9, 8, 7, 6, 5, 4, 3, 2, 1, 0];
-
+	let x = 3;
+	if x == 3 {
+		return;
+	}
 
     loop {
         let mut index = String::new();
@@ -57,4 +70,76 @@ fn do_stuf() {
         }
         println!("Element: [{}]", arr[i]);
     }
+}
+
+
+fn nasted_asigment() {
+	let y = {
+		let x = 3;
+		x + 3
+    };
+	println!("y = {y}");
+}
+
+fn get_num(x: u32) -> u32 {
+	x + 4
+} 
+
+fn cond_res(cond: bool) -> i32 {
+	return if cond {123} else {999};
+}
+
+fn from_loop(count: u32) -> u32 {
+	let mut i = 0;
+
+	let res = loop {
+		if i == count {
+			break 10 + i;
+		}
+		i += 1;
+	};
+	return res;
+}
+
+fn loop_lables() {
+	let mut count = 0;
+	'counting_up: loop {
+		println!("count = {count}");
+		let mut remaining = 10;
+
+		loop {
+			println!("remaining = {remaining}");
+			if remaining == 9 {
+				break;
+			}
+			if count == 2 {
+				break 'counting_up;
+			}
+			remaining -= 1;
+		}
+		count += 1;
+	}
+	println!("End count = {count}");
+}
+
+fn in_element_for() {
+	let arr = [1, 2, 3, 4, 5, 6, 7, 99];
+
+	for e in arr {
+		println!("Element: {e}");
+	}
+}
+
+fn for_range() {
+	for i in 1..5 {
+		println!("I: {i}");
+	}
+}
+
+fn fibonache(num: u64) -> u64 {
+	if num == 1 {
+		return num;
+	} else {
+		return num * fibonache(num - 1);
+	}
 }
