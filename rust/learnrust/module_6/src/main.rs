4fa173cae93e06d00f8693702d2b1cb81827199b
MaximMGG <maxhrunenko@gmail.com>
2026-04-11 11:35:53 +0300

    module_6 done

--- rust/learnrust/module_6/src/main.rs
+++ rust/learnrust/module_6/src/main.rs
@@ -31,6 +31,10 @@ fn main() {
     m.call();
 	main2();
 	main3();
+	main4();
+	main5();
+	main6();
+	main7();
 }
 
 fn main2() {
@@ -40,11 +44,26 @@ fn main2() {
 	let _absent_number: Option<i32> = None;
 }
 
+#[derive(Debug)]
+enum UsState {
+	Alabama,
+	Alaska,
+}
+
+impl UsState {
+	fn existed_in(&self, year: u16) -> bool {
+		match self {
+			UsState::Alabama => year >= 1819,
+			UsState::Alaska => year >= 1959,
+		}
+	}
+}
+
 enum Coin {
 	Penny,
 	Nickel,
 	Dime,
-	Quarter,
+	Quarter(UsState),
 }
 
 fn value_in_cents(coin: Coin) -> u8 {
@@ -55,7 +74,10 @@ fn value_in_cents(coin: Coin) -> u8 {
 		}
 		Coin::Nickel => 5,
 		Coin::Dime => 10,
-		Coin::Quarter => 25,
+		Coin::Quarter(state) => {
+			println!("State quarter from {state:?}");
+			25
+		}
 	}
 }
 
@@ -63,6 +85,107 @@ fn main3() {
 	let c = Coin::Penny;
 
 	println!("coin is: {}", value_in_cents(c));
+
+	let c = Coin::Quarter(UsState::Alabama);
+
+	println!("coin 2 is : {}", value_in_cents(c));
 }
 
+fn plus_five(x: Option<i32>) -> Option<i32> {
+	match x {
+		None => None,
+		Some(i) => Some(i + 1) 
+	}
+}
+
+
+fn main4() {
+	let five = Some(5);
+	let six = plus_five(five);
+	let none = plus_five(None);
+	println!("six: {six:?}");
+	println!("none: {none:?}");
+}
+
+fn add_fancy_hat(){}
+fn remove_fancy_hat(){}
+fn reroll(){}
+
+
+fn main5() {
+	//catch all patterns and the _ placeholder
+
+	let dice_roll = 9;
+	match dice_roll {
+		3 => add_fancy_hat(),
+		5 => remove_fancy_hat(),
+		_ => reroll(),
+		//_ => (), or it could be liki that
+	}
+}
+
+
+fn main6() {
+	println!("main6: concise control flow with if let and let...else");
+
+	let config_max = Some(3u8);
+
+	match config_max {
+		Some(max) => println!("The maximum is configured to be {max}"),
+		_ => (),
+	}
+
+	if let Some(max) = config_max {
+		println!("in If statement the maximum is configured to be {max}");
+	}
+}
+
+
+fn describe_state_quarter(coin: Coin) -> Option<String> {
+	if let Coin::Quarter(state) = coin {
+		if state.existed_in(1900) {
+			Some(format!("{state:?} is pretty old, for America!"))
+		} else {
+			Some(format!("{state:?} is relatively new"))
+		}
+	} else {
+		None
+	}
+}
+
+fn describe_state_quarter2(coin: Coin) -> Option<String> {
+	let state = if let Coin::Quarter(state) = coin {
+		state
+	} else {
+		return None
+	};
+
+	if state.existed_in(1900) {
+		Some(format!("{state:?} is pretty old, for America!"))
+	} else {
+		Some(format!("{state:?} is relatively new."))
+	}
+}
+
+fn describe_state_quarter3(coin: Coin) -> Option<String> {
+	let Coin::Quarter(state) = coin else {
+		return None
+	};
+
+	if state.existed_in(1900) {
+		Some(format!("{state:?} is pretty old, for America!"))
+	} else {
+		Some(format!("{state:?} is relatively new."))
+	}
+}
+
+fn main7() {
+	let c = Coin::Quarter(UsState::Alabama);
+	let some = describe_state_quarter(c);
+
+	match some {
+		Some(t) => println!("Describe state: {t}"),
+		None => println!("Descibtion doesn't exists"),
+	}
+}
 
