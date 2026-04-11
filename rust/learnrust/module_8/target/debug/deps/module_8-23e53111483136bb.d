d717d06c349d90bcbc9ab8b41c0eb0d520fc03f5
MaximMGG <maxhrunenko@gmail.com>
2026-04-11 16:10:07 +0300

    cleanup

--- rust/learnrust/module_8/target/debug/deps/module_8-23e53111483136bb.d
+++
@@ -1,5 +0,0 @@
-/home/maxim/learnpr/rust/learnrust/module_8/target/debug/deps/module_8-23e53111483136bb.d: src/main.rs
-
-/home/maxim/learnpr/rust/learnrust/module_8/target/debug/deps/libmodule_8-23e53111483136bb.rmeta: src/main.rs
-
-src/main.rs:
