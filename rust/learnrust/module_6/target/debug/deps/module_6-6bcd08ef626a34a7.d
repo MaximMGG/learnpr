8197ad43d3d0db2475c67721af377c2518db2659
MaximMGG <maxhrunenko@gmail.com>
2026-04-16 19:15:53 +0300

    compleat few more rustlings modules

---
+++ rust/learnrust/module_6/target/debug/deps/module_6-6bcd08ef626a34a7.d
@@ -0,0 +1,5 @@
+/home/maxim/learnpr/rust/learnrust/module_6/target/debug/deps/module_6-6bcd08ef626a34a7.d: src/main.rs
+
+/home/maxim/learnpr/rust/learnrust/module_6/target/debug/deps/libmodule_6-6bcd08ef626a34a7.rmeta: src/main.rs
+
+src/main.rs:
---
+++ rust/learnrust/module_6/target/debug/deps/module_6-dca471b2f4a0f3cf.d
@@ -0,0 +1,5 @@
+/home/maxim/learnpr/rust/learnrust/module_6/target/debug/deps/module_6-dca471b2f4a0f3cf.d: src/main.rs
+
+/home/maxim/learnpr/rust/learnrust/module_6/target/debug/deps/libmodule_6-dca471b2f4a0f3cf.rmeta: src/main.rs
+
+src/main.rs:
