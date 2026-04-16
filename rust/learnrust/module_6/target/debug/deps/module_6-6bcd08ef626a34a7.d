94285d9890674be1dcb6f9b71a9c05f9a209362f
MaximMGG <maxhrunenko@gmail.com>
2026-04-16 19:16:24 +0300

    cleanup

--- rust/learnrust/module_6/target/debug/deps/module_6-6bcd08ef626a34a7.d
+++
@@ -1,5 +0,0 @@
-/home/maxim/learnpr/rust/learnrust/module_6/target/debug/deps/module_6-6bcd08ef626a34a7.d: src/main.rs
-
-/home/maxim/learnpr/rust/learnrust/module_6/target/debug/deps/libmodule_6-6bcd08ef626a34a7.rmeta: src/main.rs
-
-src/main.rs:
--- rust/learnrust/module_6/target/debug/deps/module_6-dca471b2f4a0f3cf.d
+++
@@ -1,5 +0,0 @@
-/home/maxim/learnpr/rust/learnrust/module_6/target/debug/deps/module_6-dca471b2f4a0f3cf.d: src/main.rs
-
-/home/maxim/learnpr/rust/learnrust/module_6/target/debug/deps/libmodule_6-dca471b2f4a0f3cf.rmeta: src/main.rs
-
-src/main.rs:
