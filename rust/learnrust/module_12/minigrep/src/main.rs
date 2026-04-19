d930f10476b62718bd7d4ace1bb57a597d809a16
MaximMGG <maxhrunenko@gmail.com>
2026-04-19 17:02:46 +0300

    module_12 done, lots things repeted

--- rust/learnrust/module_12/minigrep/src/main.rs
+++ rust/learnrust/module_12/minigrep/src/main.rs
@@ -1,20 +1,65 @@
 
 
 use std::env;
+use std::fs;
+use std::error::Error;
+use minigrep::search;
+use minigrep::search_case_insensitive;
 
 fn main() {
 
     let args: Vec<String> = env::args().collect();
 
-    let query = &args[1];
-    let file_path = &args[2];
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        eprintln!("Problem parseing arguments: {err}");
+        std::process::exit(1);
+    });
 
-    println!("Searching for {query}");
-    println!("In file {file_path}");
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.file_path);
 
-
-    let contents = std::fs::read_to_string(file_path).expect("Should have been able to read the file");
-    println!("With text:\n{contents}");
-
-
+    if let Err(e) = run(config) {
+        eprintln!("Application error: {e}");
+        std::process::exit(1);
+    }
 }
+
+
+fn run(config: Config) -> Result<(), Box<dyn Error>>{
+    let contents = fs::read_to_string(config.file_path)?;
+
+
+    let result = if config.ignore_case {
+        search_case_insensitive(&config.query, &contents)
+    } else {
+        search(&config.query, &contents)
+    };
+
+    for res in &result {
+        println!("{res}");
+    }
+
+    Ok(())
+}
+
+struct Config {
+    query: String,
+    file_path: String,
+    ignore_case: bool,
+}
+
+impl Config {
+
+    fn build(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("Not enough arguments")
+        }
+
+        Ok(Config {
+            query: args[1].clone(),
+            file_path: args[2].clone(),
+            ignore_case: std::env::var("IGNORE_CASE").is_ok()
+        })
+    }
+}
+
