e0b05f7b54b37d4b800143366f22b1300593f5df
MaximMGG <maxhrunenko@gmail.com>
2026-04-26 14:13:49 +0300

    Box<T> and my own wersion of the Smart Pointer but with stack memeory location

--- rust/learnrust/module_15/box_main/src/main.rs
+++ rust/learnrust/module_15/box_main/src/main.rs
@@ -1,4 +1,7 @@
 
+
+use List::{Cons, Nil};
+
 fn main() {
     println!("Box<T>");
 
@@ -6,5 +9,53 @@ fn main() {
 
     println!("a = {a}");
 
-    let mut arr = Box::<[i32]>::new_zeroed_slice(3);
+    let list = Cons(1, Box::new(Cons(2, Box::new(Cons(3, Box::new(Nil))))));
+
+    println!("{:?}", list);
+
+    let mut l = &list;
+    loop {
+        match l {
+            Cons(i, e) => {
+                println!("{i}");
+                l = e;
+            },
+            Nil => {break;}
+        }
+    }
+
+    treating_smart_pointer();
+}
+
+#[derive(Debug)]
+enum List {
+    Cons(i32, Box<List>),
+    Nil,
+}
+
+
+fn treating_smart_pointer() {
+    let x = 5;
+    let y = MyBox::new(x);
+
+    assert_eq!(5, x);
+    assert_eq!(5, *y);
+}
+
+struct MyBox<T>(T);
+
+impl<T> MyBox<T> {
+    fn new(x: T) -> MyBox<T> {
+        MyBox(x)
+    }
+}
+
+use std::ops::Deref;
+
+impl<T> Deref for MyBox<T> {
+    type Target = T;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
 }
