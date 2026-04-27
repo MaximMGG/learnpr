5cc4916db328baf2bfb4c0f11eb312fbd32593a1
MaximMGG <maxhrunenko@gmail.com>
2026-04-27 22:31:32 +0300

    RefCell examples

--- rust/learnrust/module_15/rc_main/src/main.rs
+++ rust/learnrust/module_15/rc_main/src/main.rs
@@ -1,10 +1,10 @@
 
-
+use std::cell::RefCell;
 
 
 #[derive(Debug)]
 enum List<T> {
-    Cons(T, Rc<List<T>>),
+    Cons(Rc<RefCell<T>>, Rc<List<T>>),
     Nil,
 }
 
@@ -13,20 +13,18 @@ use crate::List::{Cons, Nil};
 use std::rc::Rc;
 
 fn main() {
-    println!("Rc<T> -> Reference-Counted Smart Pointer");
+    let value = Rc::new(RefCell::new(5));
 
-    let a = Rc::new(Cons(5, Rc::new(Cons(10, Rc::new(Nil)))));
-    println!("Count after creating a = {}", Rc::strong_count(&a));
-    let b = Cons(3, Rc::clone(&a));
-    println!("Count after creating b = {}", Rc::strong_count(&a));
-    {
-        let c = Rc::new(Cons(2, Rc::clone(&a)));
-        println!("Count after creating c = {}", Rc::strong_count(&a));
+    let a = Rc::new(Cons(Rc::clone(&value), Rc::new(Nil)));
 
-        let d = Cons(1, Rc::clone(&c));
+    let b = Cons(Rc::new(RefCell::new(3)), Rc::clone(&a));
 
-        println!("Count after creating d = {}", Rc::strong_count(&a));
+    let c = Cons(Rc::new(RefCell::new(4)), Rc::clone(&a));
 
-    }
-    println!("Count after c goes out of scope = {}", Rc::strong_count(&a));
+
+    *value.borrow_mut() += 10;
+
+    println!("a after = {a:?}");
+    println!("a after = {b:?}");
+    println!("a after = {c:?}");
 }
