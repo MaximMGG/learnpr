8d45cb05e14c1f23ab69b96b9de27822e8f5a3b3
MaximMGG <maxhrunenko@gmail.com>
2026-05-18 23:21:20 +0300

    create own error type

--- rust/prog_rust/chapter_7/src/main.rs
+++ rust/prog_rust/chapter_7/src/main.rs
@@ -1,5 +1,7 @@
 
 use std::io;
+use std::path::Path;
+use std::fs;
 
 fn main() {
     let r = std::ops::Range{start: 1, end: 20};
@@ -31,3 +33,33 @@ struct WeatherReport {
 fn get_weather(location: String) -> Result<WeatherReport, io::Error> {
     Ok(WeatherReport{text: format!("Weather in {} cool", location)})
 }
+
+
+fn move_all(src: &Path, dst: &Path) -> io::Result<()> {
+    for entry_result in src.read_dir()? {
+        let entry = entry_result?;
+        let dst_file = dst.join(entry.file_name());
+        fs::rename(entry.path(), dst_file)?;
+    }
+    Ok(())
+}
+
+
+use thiserror::Error; // but for this i should use crate thiserror [cargo add thiserror]
+//custom errors
+#[derive(Error, Debug)]
+//#[error("{message:} ({line:}, {column})")]
+struct JsonError {
+    pub message: String,
+    pub line: usize,
+    pub column: usize,
+}
+
+impl std::fmt::Display for JsonError {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> Result<(), std::fmt::Error> {
+        write!(f, "{} ({}:{})", self.message, self.line, self.column)
+    }
+}
+
+impl std::error::Error for JsonError {}
+
