186bf8bb2f54f6e8eee5efe92e569fa147f8d675
MaximMGG <maxhrunenko@gmail.com>
2026-05-22 19:06:32 +0300

    impl structs and methods of structs

--- rust/prog_rust/chapter_9/src/main.rs
+++ rust/prog_rust/chapter_9/src/main.rs
@@ -1,4 +1,5 @@
 
+use std::rc::Rc;
 
 #[derive(Debug)]
 struct Grayscale {
@@ -34,6 +35,8 @@ fn chop(b: Broom) -> (Broom, Broom) {
     (broom1, broom2)
 }
 
+#[derive(Debug)]
+struct Bounds(usize, usize);
 
 fn main() {
 
@@ -61,6 +64,100 @@ fn main() {
     assert_eq!(hokey2.name, "Hokey II");
     assert_eq!(hokey2.height, 30);
     assert_eq!(hokey2.health, 100);
+
+    let image_bounds = Bounds(1024, 768);
+    assert_eq!(image_bounds.0 * image_bounds.1, 786432);
+    println!("image bounds: {:?}", image_bounds);
+
+    let mut q = Queue{older: Vec::new(), younger: Vec::new()};
+
+    q.push('0');
+    q.push('1');
+    assert_eq!(q.pop(), Some('0'));
+
+    q.push('*');
+
+    assert_eq!(q.pop(), Some('1'));
+    assert_eq!(q.pop(), Some('*'));
+    assert_eq!(q.pop(), None);
+
+    assert!(q.is_empty());
+    q.push('!');
+    assert!(!q.is_empty());
+    _ = q.pop();
+
+    q.push('P');
+    q.push('D');
+    assert_eq!(q.pop(), Some('P'));
+    q.push('X');
+    
+    let (older, younger) = q.split();
+
+    assert_eq!(older, vec!['D']);
+    assert_eq!(younger, vec!['X']);
+
+    let mut bq = Box::new(Queue::new());
+    bq.push('Q');
+    assert!(!bq.is_empty());
+
+    let mut parent = Node::new("parent node");
+    let shader_node = Rc::new(Node::new("first"));
+    shader_node.append_to(&mut parent);
+}
+
+
+struct Node {
+    tag: String,
+    childer: Vec<Rc<Node>>
+}
+
+impl Node {
+    fn new(tag: &str) -> Node {
+        Node {
+            tag: tag.to_string(),
+            childer: vec![]
+        }
+    }
+
+    fn append_to(self: Rc<Self>, parent: &mut Node) {
+        parent.childer.push(self);
+    }
+}
+
+
+pub struct Queue {
+    older: Vec<char>,
+    younger: Vec<char>
+}
+
+impl Queue {
+    pub fn push(&mut self, c: char) {
+        self.younger.push(c);
+    }
+
+    pub fn new() -> Queue {
+        Queue{older: Vec::new(), younger: Vec::new()}
+    }
+
+    pub fn pop(&mut self) -> Option<char> {
+        if self.older.is_empty() {
+            if self.younger.is_empty() {
+                return None;
+            }
+            use std::mem::swap;
+            swap(&mut self.older, &mut self.younger);
+            self.older.reverse();
+        }
+        self.older.pop()
+    }
+
+    pub fn is_empty(&self) -> bool {
+        self.older.is_empty() && self.younger.is_empty()
+    }
+
+    pub fn split(self) -> (Vec<char>, Vec<char>) {
+        (self.older, self.younger)
+    }
 }
 
 
