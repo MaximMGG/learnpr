2826c9ba041185cd7c726905a123b5db1a867437
MaximMGG <maxhrunenko@gmail.com>
2026-05-24 20:33:40 +0300

    chapter_11 traits implementation

--- rust/prog_rust/chapter_11/src/main.rs
+++ rust/prog_rust/chapter_11/src/main.rs
@@ -34,6 +34,45 @@ fn main() {
             println!("{e}");
         }
     }
+
+    let v1 = (0..1000).collect::<Vec<i32>>();
+    top_ten(&v1);
+    let sum_10: i32 = v1[0..10].iter().sum();
+    println!("Sum of first 10 elements from v1 is: {sum_10}");
+
+    assert_eq!('$'.is_emoji(), false);
+
+    //let o = a.clone();
+    let line = "It is the line of some text";
+
+    let words: Vec<String> = 
+        //line.split_whitespace().map(<str as ToString>::to_string).collect();
+        //line.split_whitespace().map(ToString::to_string).collect();
+        line.split_whitespace().map(|x| x.to_string()).collect();
+    //all under the the same
+    
+    for word in words {
+        println!("{word}");
+    }
+}
+
+use std::hash::Hash;
+use std::fmt::Debug;
+//use std::ops::Add;
+use std::ops::AddAssign;
+
+fn top_ten<T: Debug + Hash + Eq + AddAssign + Copy>(values: &Vec<T>) {
+    if values.len() < 10 {
+        return;
+    }
+
+    let mut sum: T = values[0];
+    for i in 1..10 {
+        sum += values[i];
+    }
+
+    println!("Sum of first 10 elements: {:?}", sum);
+
 }
 
 fn min<T: std::cmp::Ord>(value1: T, value2: T) -> T {
@@ -43,3 +82,41 @@ fn min<T: std::cmp::Ord>(value1: T, value2: T) -> T {
         value2
     }
 }
+
+use std::io::{Result};
+
+pub struct Sink;
+
+impl Write for Sink {
+    fn write(&mut self, buf: &[u8]) -> Result<usize> {
+        Ok(buf.len())
+    }
+    fn flush(&mut self) -> Result<()> {
+        Ok(())
+    }
+}
+
+trait IsEmoji {
+    fn is_emoji(&self) -> bool;
+}
+
+impl IsEmoji for char {
+    fn is_emoji(&self) -> bool {
+        false
+    }
+}
+
+struct HtmlDocument;
+
+trait WriteHtml {
+    fn write_html(&mut self, html: &HtmlDocument) -> Result<()>;
+}
+
+impl<W: Write> WriteHtml for W {
+    fn write_html(&mut self, html: &HtmlDocument) -> Result<()> {
+        _ = html;
+        Ok(())
+    }
+}
+
+
