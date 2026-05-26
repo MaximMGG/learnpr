142412adbf5f42bfa8a94417c8ac79051f604c3b
MaximMGG <maxhrunenko@gmail.com>
2026-05-26 23:24:51 +0300

    cleanup chapter_10

--- rust/prog_rust/chapter_10/target/debug/deps/chapter_10-ef1ca7cd7f15b6c1.d
+++
@@ -1,5 +0,0 @@
-/home/maxim/learnpr/rust/prog_rust/chapter_10/target/debug/deps/chapter_10-ef1ca7cd7f15b6c1.d: src/main.rs
-
-/home/maxim/learnpr/rust/prog_rust/chapter_10/target/debug/deps/libchapter_10-ef1ca7cd7f15b6c1.rmeta: src/main.rs
-
-src/main.rs:
