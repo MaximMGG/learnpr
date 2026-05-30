df4787d25c10fdcdb7bc3b1aa493f667f1a03cf2
MaximMGG <maxhrunenko@gmail.com>
2026-05-30 23:17:19 +0300

    myVec done, but for now without Index traits:

--- rust/test/my_vec/src/lib.rs
+++ rust/test/my_vec/src/lib.rs
@@ -2,6 +2,7 @@
 
 use std::alloc;
 use std::ptr::NonNull;
+use std::ops::Drop;
 
 pub struct MyVec<T> {
     ptr: NonNull<T>,
@@ -39,22 +40,41 @@ impl <T> MyVec<T> {
             self.capacity = 4;
             self.len = 1;
         } else if self.len == self.capacity {
-            let layout = alloc::Layout::array::<T>(self.capacity << 1).expect("Could create layout");
-            let ptr = unsafe {alloc::alloc(layout)} as *mut T;
-            let ptr = NonNull::new(ptr).expect("Could alloca some memory");
-            unsafe {ptr.as_ptr().swap(self.ptr.as_ptr());}
-            self.ptr = ptr;
-            unsafe {self.ptr.as_ptr().wrapping_add(self.len).write(val)}
-            self.capacity <<= 1;
+            let layout = alloc::Layout::from_size_align(self.capacity << 1, std::mem::align_of::<T>()).expect("layout ok");
+            let p = unsafe {alloc::realloc(self.ptr.as_ptr() as *mut u8, layout, self.capacity << 1) as *mut T};
+            self.ptr = NonNull::new(p).expect("Could realloc memory");
+            unsafe {self.ptr.as_ptr().add(self.len()).write(val)};
             self.len += 1;
+            self.capacity <<= 1;
         } else {
-            unsafe {self.ptr.as_ptr().wrapping_add(self.len).write(val)}
+            let offset = self.len.checked_mul(std::mem::size_of::<T>()).expect("Cannot reach memory location");
+            assert!(offset < isize::MAX as usize, "Wrapped isize");
+            unsafe {self.ptr.as_ptr().add(self.len()).write(val)};
             self.len += 1;
         }    
     }
+    pub fn get(&self, index: usize) -> Option<&T> {
+        if index >= self.len {
+            return None
+        }
+        Some (unsafe {
+            &(*self.ptr.as_ptr().add(index))
+        })
+    }
 }
 
-use std::ops::Index;
+impl<T> Drop for MyVec<T> {
+    fn drop(&mut self) {
+        unsafe {
+            std::ptr::drop_in_place(std::slice::from_raw_parts_mut(self.ptr.as_ptr(), self.capacity()));
+            let layout = alloc::Layout::array::<T>(self.capacity).expect("Layout error");
+            alloc::dealloc(self.ptr.as_ptr() as *mut u8, layout);
+        }
+    }
+}
+
+
+
 
 
 #[cfg(test)]
@@ -64,8 +84,8 @@ mod tests {
     #[test]
     fn it_runs() {
 
-        let a: Vec<usize> = vec![1];
-        let _b = a.index(3);
+        //let a: Vec<usize> = vec![1];
+        //let _b = a.index(3);
 
         let mut s = MyVec::<i32>::new();
         s.push(3);
@@ -73,6 +93,15 @@ mod tests {
         s.push(3);
         s.push(4);
         s.push(5);
+
+
+        for i in 0..s.len() {
+            println!("Val if index: {i} -> {}", match s.get(i) {
+                Some(t) => *t,
+                None => 0
+            });
+        }
+
 
         assert_eq!(s.len(), 5);
         assert_eq!(s.capacity(), 8);
