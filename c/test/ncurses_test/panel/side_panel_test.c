e9159eb267152401a60adebb10192d8b90688b1b
MaximMGG <maxhrunenko@gmail.com>
2026-07-02 19:43:40 +0300

    side msg threaded test

--- c/test/ncurses_test/panel/side_panel_test.c
+++ c/test/ncurses_test/panel/side_panel_test.c
@@ -2,15 +2,19 @@
 #include <panel.h>
 #include <cstdext/core.h>
 #include <unistd.h>
+#include <pthread.h>
 
 WINDOW *side_window;
 PANEL *side_panel;
 str side_msg = "You press %c letter";
 #define WIDTH 40
 #define HEIGHT 5
+pthread_mutex_t msg_m;
+
 
 
 void initWindow() {
+  pthread_mutex_init(&msg_m, null);
   initscr();
   noecho();
   cbreak();
@@ -30,9 +34,12 @@ void shutdownWindow() {
   del_panel(side_panel);
   delwin(side_window);
   endwin();
+  pthread_mutex_destroy(&msg_m);
 }
 
-void showMsg(i8 c) {
+ptr showMsgHelper(ptr p) {
+  pthread_mutex_lock(&msg_m);
+  i8 c = *cast(i8 *, p);
   show_panel(side_panel);
   update_panels();
   doupdate();
@@ -45,6 +52,17 @@ void showMsg(i8 c) {
   hide_panel(side_panel);
   update_panels();
   doupdate();
+  DEALLOC(p);
+  pthread_mutex_unlock(&msg_m);
+  return null;
+}
+
+void showMsg(i8 c) {
+  pthread_t w;
+  i8 *p = ALLOC(1);
+  *p = c;
+  pthread_create(&w, null, showMsgHelper, p);
+  pthread_detach(w);
 }
 
 int main() {
