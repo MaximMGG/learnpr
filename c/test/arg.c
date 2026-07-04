311ff889cc7582047d8150ce2393f4ad1f79e2c3
MaximMGG <maxhrunenko@gmail.com>
2026-07-04 20:59:48 +0000

    little sublime-text experiments

--- c/test/arg.c
+++ c/test/arg.c
@@ -1,16 +1,17 @@
 #include <stdio.h>
 #include <stdarg.h>
 
+void print_num(int a);
 
 void arg_print(int a, ...) {
   va_list li;
-  va_start(li, a);
+  va_start(li, a);  
 
-  char buf[4] = {0};
-
-  int res = va_arg(li, char);
+  int res = (int)va_arg(li, int);
+  print_num(res);
+  va_end(li);
 }
 
 void print_num(int a) {
-  printf("%d\n", a);
+  printf("%d", a);
 }
