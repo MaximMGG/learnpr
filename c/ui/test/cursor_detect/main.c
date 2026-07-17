e3a523a6810ede9148484b493e77d2bc113fcb63
MaximMGG <maxhrunenko@gmail.com>
2026-07-17 21:17:35 +0300

    C OpenGL test

--- c/ui/test/cursor_detect/main.c
+++ c/ui/test/cursor_detect/main.c
@@ -1,8 +1,12 @@
 #include <cglm/cglm.h>
+#include <cglm/quat.h>
 #include <glad/glad.h> 
 #include <GLFW/glfw3.h>
 #include <cstdext/core.h>
 #include <cstdext/io/logger.h>
+#include "shader.h"
+#include "texture.h"
+#include <cglm/cglm.h>
 
 
 #define WIDTH 1280
@@ -15,6 +19,16 @@ void init_logger() {
 
 void deinit_logger() {
   logCleanup();
+}
+
+void framebuffer_callback(GLFWwindow *window, i32 width, i32 height) {
+  glViewport(0, 0, width, height);
+}
+
+void process_input(GLFWwindow *window) {
+  if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
+    glfwSetWindowShouldClose(window, true);
+  }
 }
 
 
@@ -30,13 +44,117 @@ i32 main() {
   glfwWindowHint(GLFW_VERSION_MINOR, 3);
   glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-  mat4 ortho = GLM_MAT4_IDENTITY;
-  glm_ortho(0, cast(f32, WIDTH), 0, cast(f32, HEIGHT), -1, 100, ortho);
-
-
   glfwMakeContextCurrent(window);
+  gladLoadGLLoader((GLADloadproc)glfwGetProcAddress);
+  glEnable(GL_DEPTH_TEST);
 
+  Program p = programLoad("vertex.glsl", "fragment.glsl");
+  if (p == 0) {
+    log(ERROR, "programLoad error");
+  }
+  Texture t1 = textureLoadJpg("container.jpg");
+  if (t1 == 0) {
+    log(ERROR, "textureLoadJpg error");
+  }
+  Texture t2 = textureLoadPng("awesomeface.png");
+  if (t2 == 0) {
+    log(ERROR, "textureLoadPng error");
+  }
 
+  f32 vertices[] = {
+    -0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
+     0.5f, -0.5f, -0.5f,  1.0f, 0.0f,
+     0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+     0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+    -0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
+    -0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
+
+    -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+     0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+     0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
+     0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
+    -0.5f,  0.5f,  0.5f,  0.0f, 1.0f,
+    -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+
+    -0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+    -0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+    -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+    -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+    -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+    -0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+
+     0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+     0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+     0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+     0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+     0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+     0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+
+    -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+     0.5f, -0.5f, -0.5f,  1.0f, 1.0f,
+     0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+     0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+    -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+    -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+
+    -0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
+     0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+     0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+     0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+    -0.5f,  0.5f,  0.5f,  0.0f, 0.0f,
+    -0.5f,  0.5f, -0.5f,  0.0f, 1.0f
+  };
+
+  u32 VBO, VAO;
+  glGenVertexArrays(1, &VAO);
+  glGenBuffers(1, &VBO);
+  glBindVertexArray(VAO);
+
+  glBindBuffer(GL_ARRAY_BUFFER, VBO);
+  glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(f32), (ptr)0);
+  glEnableVertexAttribArray(0);
+  
+  glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(f32), (ptr)(3 * sizeof(f32)));
+  glEnableVertexAttribArray(1);
+
+  programUse(p);
+  programSetInt(p, "texture1", 0);
+  programSetInt(p, "texture2", 1);
+
+  while(!glfwWindowShouldClose(window)) {
+    process_input(window);
+    glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+    mat4 model = GLM_MAT4_IDENTITY_INIT;
+    mat4 view = GLM_MAT4_IDENTITY_INIT;
+    mat4 projection = GLM_MAT4_IDENTITY_INIT;
+
+    glm_rotate(model, glfwGetTime(), (vec3){0.5, 1.0, 0.0});
+    glm_translate(view, (vec3){0.0, 0.0, -3.0});
+    glm_perspective(glm_rad(45.0f), cast(f32, WIDTH) / cast(f32, HEIGHT), 0.1, 100.0, projection);
+
+    programUse(p);
+    programSetMat4(p, "model", model);
+    programSetMat4(p, "view", view);
+    programSetMat4(p, "projection", projection);
+
+    glBindVertexArray(VAO);
+    glDrawArrays(GL_TRIANGLES, 0, 36);
+    
+    glfwSwapBuffers(window);
+    glfwPollEvents();
+  }
+
+  programDestroy(p);
+  textureDestroy(t1);
+  textureDestroy(t2);
+  glfwDestroyWindow(window);
+  glfwTerminate();
+  log(INFO, "End of OpenGL");
+  logCleanup();
 
   return 0;
 }
