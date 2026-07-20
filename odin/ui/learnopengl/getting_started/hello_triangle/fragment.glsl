985ccd911ea335f1032ef8c5bbbc7a0c4a0a92b2
MaximMGG <maxhrunenko@gmail.com>
2026-07-20 11:29:58 +0000

    cirle drawing done!

--- odin/ui/learnopengl/getting_started/hello_triangle/fragment.glsl
+++ odin/ui/learnopengl/getting_started/hello_triangle/fragment.glsl
@@ -1,20 +1,15 @@
 #version 330 core
 
 out vec4 FragColor;
-in vec2 fragCoord;
 
 void main() {
-  vec2 resolution = vec2(200.0, 100.0);
-  vec2 uv = fragCoord/resolution.xy * 2.0 - 1.0;
+  vec2 resolution = vec2(640.0, 480.0);
+  vec2 uv = gl_FragCoord.xy/resolution.xy * 2.0 - 1.0;
   float aspect = resolution.x / resolution.y;
   uv.x *= aspect;
 
-
-  float distance = distance(vec2(0.0), uv);
-  if (distance > 0.0) {
-    distance = 1.0;
-  }
-
+  float distance = 1.0 - length(uv);
+  distance = step(0.0, distance);
   FragColor.rgb = vec3(distance);
 
   // FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);
--- odin/ui/learnopengl/getting_started/hello_triangle/vertex.glsl
+++ odin/ui/learnopengl/getting_started/hello_triangle/vertex.glsl
@@ -1,11 +1,6 @@
 #version 330 core
 layout (location = 0) in vec3 aPos;
 
-
-out vec2 fragCoord;
-
-
 void main() {
   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);
-  fragCoord = aPos.xy;
 }
