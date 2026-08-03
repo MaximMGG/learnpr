aed45da1fda3c224de1dd09d347736247353bb77
MaximMGG <maxhrunenko@gmail.com>
2026-08-03 20:20:33 +0000

    Nim's templates

--- nim/tutor/generic.nim
+++ nim/tutor/generic.nim
@@ -1,3 +1,4 @@
+import std/math
 
 type BinaryTree*[T] = ref object
   le, ri: BinaryTree[T]
@@ -47,3 +48,51 @@ add(root, newNode("World"))
 
 for str in root.preoder():
   stdout.writeLine(str)
+
+
+echo "Tempalates"
+
+template `!=` (a, b: untyped): untyped =
+  not (a == b)
+
+assert(7 != 3)
+
+const debug = true
+
+template log(msg: string) =
+  if debug: stdout.writeLine(msg)
+
+var x = 4
+log("x has the value: " & $x)
+
+template withFile(f: untyped, filename: string, mode: FileMode, body: untyped) =
+  let fn = filename
+  var f: File
+  if open(f, fn, mode):
+    try:
+      body
+    finally:
+      close(f)
+  else:
+    quit("Cannot open: " & fn)
+
+
+withFile(txt, "ttempl3.txt", fmWrite):
+  txt.writeLine("line 1")
+  txt.writeLine("line 2")
+
+
+
+template liftScalarProc(fname) =
+
+
+  proc fname[T](x: openarray[T]): auto =
+    var temp: T
+    type outType = typeof(fname(temp))
+    result = newSeq[outType](x.len)
+    for i in 0 ..< x.len:
+      result[i] = fname(x[i])
+
+
+liftScalarProc(sqrt)
+echo sqrt(@[4.0, 16.0, 25.0, 36.0])
