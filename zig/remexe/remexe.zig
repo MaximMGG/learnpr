96c21699f5b3e2dba6c05cf700ac9fdf342dd074
MaximMGG <maxhrunenko@gmail.com>
2026-08-11 20:14:25 +0000

    rewrite zig remexe implementation

--- zig/remexe/remexe.zig
+++ zig/remexe/remexe.zig
@@ -11,36 +11,42 @@ fn check_extension(file_name: []const u8) bool {
     }
 }
 
+fn print_level(level: i32) !void {
+    for (0..@as(usize, @intCast(level))) |_| {
+        try stdout.print(" ", .{});
+    }
+}
 
-fn iterate_throuth_dir(allocator: std.mem.Allocator, dir: std.fs.Dir, path: []const u8) !void {
+fn iterate_throuth_dir(allocator: std.mem.Allocator, dir: std.Io.Dir, path: []const u8, io: std.Io, level: i32) !void {
     var it = dir.iterate();
+    try print_level(level);
     try stdout.print("Scanning {s}\n", .{path});
 
-    while(try it.next()) |entry| {
+    while(try it.next(io)) |entry| {
         switch(entry.kind) {
             .directory => {
                 if (entry.name[0] == '.') {
                     continue;
                 }
 
-                var sub_dir = try dir.openDir(entry.name, .{.iterate = true});
-                defer sub_dir.close();
+                var sub_dir = try dir.openDir(io, entry.name, .{.iterate = true});
+                defer sub_dir.close(io);
                 const new_path = try std.mem.concat(allocator, u8, &[_][]const u8{path,"/", entry.name});
                 defer allocator.free(new_path);
-                try iterate_throuth_dir(allocator, sub_dir, new_path);
+                try iterate_throuth_dir(allocator, sub_dir, new_path, io, level + 1);
             },
             .file => {
-                const f = try dir.openFile(entry.name, .{});
-                const m = try f.mode();
-                if ((@as(u64, @intCast(m)) & std.posix.S.IXUSR) > 0) {
-                    f.close();
+                const f = try dir.openFile(io, entry.name, .{ .mode =  .read_only});
+                const stat = try f.stat(io);
+                const mode = stat.permissions.toMode();
+
+                if ((mode & 0o111) != 0) {
                     if (check_extension(entry.name)) {
-                        try stdout.print("Deleting executable {s}\n", .{entry.name});
-                        try dir.deleteFile(entry.name);
+                        try print_level(level);
+                        try stdout.print("\x1b[2;31mDeleting executable {s}\x1b[0m\n", .{entry.name});
+                        try dir.deleteFile(io, entry.name);
                     }
-                } else {
-                    f.close();
-                }
+                }                 
             },
             else => {continue;}
         }
@@ -49,10 +55,10 @@ fn iterate_throuth_dir(allocator: std.mem.Allocator, dir: std.fs.Dir, path: []co
 
 
 
-pub fn main() !void {
+pub fn main(init: std.process.Init) !void {
 
     var buf: [1024]u8 = undefined;
-    var f_stdout = std.fs.File.stdout().writer(&buf);
+    var f_stdout = std.Io.File.stdout().writer(init.io, &buf);
     stdout = &f_stdout.interface;
 
 
@@ -60,10 +66,10 @@ pub fn main() !void {
     // const allocator = gpa.allocator();
     const c_allocator = std.heap.c_allocator;
 
-    var cwd = try std.fs.cwd().openDir(".", .{.iterate = true});
-    defer cwd.close();
+    var cwd = try std.Io.Dir.cwd().openDir(init.io, ".", .{.iterate = true});
+    defer cwd.close(init.io);
     //try iterate_throuth_dir(allocator, cwd, ".");
-    try iterate_throuth_dir(c_allocator, cwd, ".");
+    try iterate_throuth_dir(c_allocator, cwd, ".", init.io, 0);
 
     try stdout.flush();
 }
